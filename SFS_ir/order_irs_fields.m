0ad6c8a690c73c9c0e21d03a9c3ef7006b42763e
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-02-08 17:00:10 +0000

    Changed reordering of fields for irs structs
    Now it can also handle structs with more than the standard fields
    
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40297

--- SFS_ir/order_irs_fields.m
+++ SFS_ir/order_irs_fields.m
@@ -9,9 +9,11 @@ function irs = order_irs_fields(irs)
 %   Output options
 %       irs - irs struct with the fields in the order as in new_irs()
 %
-%   ORDER_IRS_FIELDS() reorders the fields in the given irs according to the
+%   ORDER_IRS_FIELDS(irs) reorders the fields in the given irs according to the
 %   order given in the new_irs function, which is our reference implementation
-%   for the irs format.
+%   for the irs format. If the given irs struct contains more fields, the
+%   additional fields will be appended at the end of the struct in the order
+%   given in the original struct.
 %
 %   See also: new_irs, check_irs, IR_format.txt
 
@@ -32,4 +34,29 @@ warning('on','SFS:irs_fields_order');
 % Get the reference implementation of the irs format and reorder the fields of
 % the given irs according to the reference implementation.
 ref_irs = new_irs();
-irs = orderfields(irs,ref_irs);
+% Get fields
+ref_fields = fieldnames(ref_irs);
+% Get the fields for the given irs
+fields = fieldnames(irs);
+% If the number of fields is identical sort directly
+if length(ref_fields)==length(fields)
+    irs = orderfields(irs,ref_fields);
+else
+   % Get the indices of the positions of the ref_fields in the given irs
+    idx = [];
+    for ii = 1:length(ref_fields)
+        for jj = 1:length(fields)
+            if strcmp(fields{jj},ref_fields{ii})
+                idx = [idx jj];
+            end
+        end
+    end
+    % Get the indices for additional fields
+    for jj = 1:length(fields)
+        if ~any(idx==jj)
+            idx = [idx jj];
+        end
+    end
+    sorted_fields = fields(idx);
+    irs = orderfields(irs,sorted_fields);
+end

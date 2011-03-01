03e6bc6e114fd1b249bd06e607e135fa6b74f3dd
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-03-01 16:58:00 +0000

    Added missing helper function
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40315

---
+++ SFS_helper/isargchar.m
@@ -0,0 +1,21 @@
+function isargchar(varargin)
+%ISARGCHAR tests if the given arg is a char and returns an error otherwise
+%   Usage: isargstruct(arg1,arg2,...)
+%
+%   Input options:
+%       args        - list of args
+%
+%   ISARGCHAR(args) tests if all given args are a char and returns 
+%   an error otherwise.
+%
+%   see also: isargstruct
+
+% AUTHOR: Hagen Wierstorf
+
+
+%% ===== Checking for struct =============================================
+for ii = 1:nargin
+    if ~ischar(varargin{ii})
+        error('%s need to be a string.',inputname(ii));
+    end
+end

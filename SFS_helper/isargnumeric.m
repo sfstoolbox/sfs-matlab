da2b6584696740006483fb461ee45b05e66da837
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-09-05 12:52:33 +0000

    Added another helper function
    --HG--
    rename : SFS_helper/isargscalar.m => SFS_helper/isargnumeric.m
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40453

---
+++ SFS_helper/isargnumeric.m
@@ -0,0 +1,21 @@
+function isargnumeric(varargin)
+%ISARGNUMERIC tests if the given arg is numeric and returns an error otherwise
+%   Usage: isargnumeric(arg1,arg2,...)
+%
+%   Input options:
+%       args        - list of args
+%
+%   ISARGNUMRIC(args) tests if all given args are numeric and returns
+%   an error otherwise.
+%
+%   see also: isargscalar
+
+% AUTHOR: Hagen Wierstorf
+
+
+%% ===== Checking for numeric value ======================================
+for ii = 1:nargin
+    if ~isnumeric(varargin{ii})
+        error('%s need to be numeric.',inputname(ii));
+    end
+end

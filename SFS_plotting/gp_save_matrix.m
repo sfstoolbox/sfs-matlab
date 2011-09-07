8120b6eb1511d60d8ace6e5bfad4c9842776ce02
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-09-07 10:31:33 +0000

    Fixed a missing ;
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40459

--- SFS_plotting/gp_save_matrix.m
+++ SFS_plotting/gp_save_matrix.m
@@ -36,7 +36,7 @@ MS = zeros(length(x)+1,length(y)+1);
 MS(1,1) = length(x);
 MS(1,2:end) = y;
 MS(2:end,1) = x;
-MS(2:end,2:end) = M'
+MS(2:end,2:end) = M';
 
 % Write data into the file
 fid = fopen(file,'w');

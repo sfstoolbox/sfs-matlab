ad05a5ad54c0da9c819a937a92def838fe5f8509
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-10-28 13:53:55 +0000

    Added a script to plot different parameters of given irs sets
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40542

---
+++ scripts/check_irs_data.m
@@ -0,0 +1,32 @@
+% Checks all irs data sets in the current directory, by plotting the relevant
+% data
+
+files = dir('*.mat');
+
+for ii = 1:length(files)
+
+    fprintf(1,'\n\nTesting dataset: %s\n',files(ii).name);
+
+    irs = read_irs(files(ii).name);
+
+    % Plot the whole data set
+    figure; imagesc(irs.left(1:1500,:)); title('Left Ear');
+    figure; imagesc(irs.right(1:1500,:)); title('Right Ear');
+
+    % Plot impulse response
+    figure; plot(irs.left(1:1500,100),'b-',irs.right(1:1500,100),'r-');
+
+    % Extract ITD and ILD
+    %itd = extract_itd(irs.left,irs.right,irs.fs);
+    ild = extract_ild(irs.left,irs.right);
+    %plot_itd(itd);
+    angle1 = degree(irs.apparent_azimuth(1))
+    angle2 = degree(irs.apparent_azimuth(end))
+    length(irs.apparent_azimuth)
+    %plot_ild(phi,ild);
+    figure; plot(degree(irs.apparent_azimuth),ild);
+
+    key_in = input('Resume to next by pressing Enter');
+    close all;
+
+end

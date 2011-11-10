65394173a90504b4fae52066a67b2d658cd76037
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-11-10 14:25:35 +0000

    Added a function to add the irs path to the Matlab path
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40569

---
+++ SFS_helper/addirspath.m
@@ -0,0 +1,30 @@
+function addirspath(basepath)
+%ADDIRSPATH adds the directory containing irs files to the path
+%
+%   Usage: addirspath(basepath)
+%
+%   Input options:
+%       basepath - path where the databases are located. Default:
+%                  ~/svn/ir_databases
+%
+%   ADDIRSPATH(basepath) add basepath and its subdirectories to the path. If
+%   basepath is omitted, ~/svn/ir_databases is used.
+
+% AUTHOR: Hagen Wierstorf
+
+
+% ===== Checking of input parameters ====================================
+nargmin = 0;
+nargmax = 1;
+error(nargchk(nargmin,nargmax,nargin));
+if nargin==nargmin
+    basepath = '~/svn/ir_databases';
+else
+    isargdir(basepath);
+end
+
+
+%% ===== Adding pathes ==================================================
+% FIXME: this adds also all .svn subdirectories!
+path = genpath(basepath);
+addpath(path,'-end');

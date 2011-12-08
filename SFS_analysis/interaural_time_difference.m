291ccfd59cd9993a663a9f51626facfa798769cc
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2011-12-08 16:21:23 +0000

    Corrected the comments of the interaural_time_difference function by removing Tabs
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40600

--- SFS_analysis/interaural_time_difference.m
+++ SFS_analysis/interaural_time_difference.m
@@ -2,23 +2,23 @@ function [itd,idxleft,idxright] = interaural_time_difference(insigleft,insigrigh
 %INTERAURAL_TIME_DIFFERENCE Extract the ITD between the two given signals
 %
 %   Usage: itd = interaural_time_difference(insigleft,insigright,fs)
-%   	   itd = interaural_time_difference(insigleft,insigright,fs,mode)
+%          itd = interaural_time_difference(insigleft,insigright,fs,mode)
 %
 %   Input parameters:
 %       insigleft   - left ear signal. This can also be a matrix containing
 %                     left signals for different frequency bands
 %       insigright  - the same as insigleft, but for the right ear
 %       fs          - sampling rate
-%		mode		- describes which algorith was used: simple is default
-%					- you should use 'int' too and compare results
-%					- 'simple'		- fast, use only for low frequencies (default)
-%					- 'hilbert' 	- use only for hight frequencies
-%					- 'int'         - use if simple and hilbert fail
-%					- 'int2'		- use for signals with high noise
-%					- 'int3'		- use if int fails
-%					- 'max'			- very primitive algorithm, doesn't work well
-%		fit			- seconds the itd from angle i can difer from itd of angle i+1 
-%		              (default is 0.1 )
+%       mode        - describes which algorith was used: simple is default
+%                   - you should use 'int' too and compare results
+%                   - 'simple'      - fast, use only for low frequencies (default)
+%                   - 'hilbert'     - use only for hight frequencies
+%                   - 'int'         - use if simple and hilbert fail
+%                   - 'int2'        - use for signals with high noise
+%                   - 'int3'        - use if int fails
+%                   - 'max'         - very primitive algorithm, doesn't work well
+%       fit         - seconds the itd from angle i can difer from itd of angle i+1
+%                     (default is 0.1 )
 %
 %   Output parameters:
 %       itd         - ITD for the given signals. A single value for two
@@ -48,18 +48,18 @@ if nargin==5
     isargchar(mode);
 elseif nargin==4
     isargchar(mode);
-	fit=0.1;
+    fit=0.1;
 elseif nargin==3
     mode='simple';
-	fit=0.1;
+    fit=0.1;
 end
 
 
 %% ===== Computation ====================================================
-if strcmp ( mode,'hilbert') 
-	% Extract the envelope of the input signals
-	insigleft = abs(hilbert(insigleft));
-	insigright = abs(hilbert(insigright));
+if strcmp ( mode,'hilbert')
+    % Extract the envelope of the input signals
+    insigleft = abs(hilbert(insigleft));
+    insigright = abs(hilbert(insigright));
 end
 
 % See if we have more than one frequency channel in the insig
@@ -79,108 +79,108 @@ if strcmp ( mode,'hilbert') |strcmp ( mode,'simple')
     resampleft = resample(insigleft(:,ii),10*fs,fs);
     resampright = resample(insigright(:,ii),10*fs,fs);
 
-	
-	idxleft(ii) = find(abs(resampleft) > tresholdleft,1,'first');
+
+    idxleft(ii) = find(abs(resampleft) > tresholdleft,1,'first');
     idxright(ii) = find(abs(resampright) > tresholdright,1,'first');
-	itd(ii) = (idxleft(ii)-idxright(ii))/(10*fs);
-	
-	if 0 %mod(ii,30)==0
-		range = 001:8000;
-		plot(range,resampright(range),range,resampleft(range),idxleft(ii),tresholdleft,'+',idxright(ii),tresholdright,'*');
-		grid on;
-		file = sprintf('results/itd_%s_%i.png',mode,ii);
-		print(file);
-	end
-	
+    itd(ii) = (idxleft(ii)-idxright(ii))/(10*fs);
+
+    if 0 %mod(ii,30)==0
+        range = 001:8000;
+        plot(range,resampright(range),range,resampleft(range),idxleft(ii),tresholdleft,'+',idxright(ii),tresholdright,'*');
+        grid on;
+        file = sprintf('results/itd_%s_%i.png',mode,ii);
+        print(file);
+    end
+
 elseif strcmp ( mode,'max')
     % Find maximum and use the maximum to calculate the ITD
     [maxleft idxleft(ii)] = max(abs(insigleft(:,ii)));
     [maxright idxright(ii)] = max(abs(insigright(:,ii)));
-	itd(ii) = (idxleft(ii)-idxright(ii))/(10*fs);
+    itd(ii) = (idxleft(ii)-idxright(ii))/(10*fs);
 
 elseif strncmp ( mode,'int',3)
-	%here we try an other approach / integration of the difference and minimize
-	difmin=0;
-	
-	a=sum(abs(insigleft(:,ii)))/sum(abs(insigright(:,ii)));			%here we normalize the both signals
-	insigright(:,ii)=a*insigright(:,ii);							
-																	%the right one should be louder
-	%put zeros in the end and beginning
-	l=(length(insigleft(:,ii)));
-	%stretch= round(0.1*l);
-	stretch = round(fs*0.001);
-	
-	newleft = zeros(1,l+2*stretch);
-	newleft(stretch+1:l+stretch) = insigleft(:,ii);
-	
-	
-	[maxright idxright] = max(abs(insigright(:,ii)));
-	
+    %here we try an other approach / integration of the difference and minimize
+    difmin=0;
 
-	weight=(idxright*2-1:-1:idxright)-idxright;
-	
-	if strcmp(mode,'int2')	
-		weight=log(weight+1);
-	end
-	
-	%weight=(1-10*(abs((1:l+2*stretch)-jminold)/l));
-	%for k = 1:l+2*stretch
-	%if weight(k)<0
-	%	weight(k)= 0;
-	%end
-	%end
-	%weight=weight.^2;
+    a=sum(abs(insigleft(:,ii)))/sum(abs(insigright(:,ii)));         %here we normalize the both signals
+    insigright(:,ii)=a*insigright(:,ii);
+                                                                    %the right one should be louder
+    %put zeros in the end and beginning
+    l=(length(insigleft(:,ii)));
+    %stretch= round(0.1*l);
+    stretch = round(fs*0.001);
 
-	%add zeros also to the end and beginning of the right signal, and let the data turn from beginning to end
-	
-	for j = 1:2*stretch
-		newright = zeros(1,l+2*stretch);						%create an empty signal of the new lenght
-		if strcmp(mode,'int3')
-			newright(j:j-1+l) = insigright(:,ii);
-		else
-			newright(j:j-1+idxright) = insigright(1:idxright,ii);	%we cut the right(shifting) signal after the maximum	
-		end
-		
-		dif = newleft - newright;
-		
-		if strcmp(mode,'int') |strcmp(mode,'int2')
-		    dif2 = zeros(1,l+2*stretch);
-			dif2(j:j-1+idxright) = dif(j:j-1+idxright); 	%only the errors before we cut the signal are important
-			%dif2(find(abs(newright)>0.05*maxright)) = dif(find(abs(newright)>0.05*maxright));	%only errors where the right signal is not zero are important
-		
-			dif=dif2;
-			dif(j:j-1+idxright)=weight.*dif(j:j-1+idxright);	%here we say which errors are more important than others
-		end
-		
-		%dif = dif.*weight*100;	
-		%dif = dif.^2;
-		dif=abs(dif);
-		sumdif = sum(dif);
+    newleft = zeros(1,l+2*stretch);
+    newleft(stretch+1:l+stretch) = insigleft(:,ii);
 
 
-		if ((sumdif < difmin) & abs(((-stretch+j)/fs)-itdold)<fit   ) | j==1
-			jmin = j;
-			difmin=sumdif;
-			difi=dif;
-			itd(ii) = (-stretch+j)/fs;
-		end
-	end
-	itdold=itd(ii);
-	jminold=jmin;
-	
-	if 0 %mod(ii,30)==0																	%plots the signals for the given angels
-		newright = zeros(1,l+2*stretch);
-		if strcmp(mode,'int3')
-			newright(jmin:jmin-1+l) = insigright(:,ii);
-		else
-			newright(jmin:jmin-1+idxright) = insigright(1:idxright,ii);	%we cut the right(shifting) signal after the maximum	
-		end
-		range = 200:1000;
-		plot(range,newright(range),range,newleft(range),range,difi(range),jmin:jmin-1+idxright,weight*0.0000002);
-		grid on;
-		file = sprintf('results/itd_%s_%i.png',mode,ii);
-		print(file);
-	end
+    [maxright idxright] = max(abs(insigright(:,ii)));
+
+
+    weight=(idxright*2-1:-1:idxright)-idxright;
+
+    if strcmp(mode,'int2')
+        weight=log(weight+1);
+    end
+
+    %weight=(1-10*(abs((1:l+2*stretch)-jminold)/l));
+    %for k = 1:l+2*stretch
+    %if weight(k)<0
+    %   weight(k)= 0;
+    %end
+    %end
+    %weight=weight.^2;
+
+    %add zeros also to the end and beginning of the right signal, and let the data turn from beginning to end
+
+    for j = 1:2*stretch
+        newright = zeros(1,l+2*stretch);                        %create an empty signal of the new lenght
+        if strcmp(mode,'int3')
+            newright(j:j-1+l) = insigright(:,ii);
+        else
+            newright(j:j-1+idxright) = insigright(1:idxright,ii);   %we cut the right(shifting) signal after the maximum
+        end
+
+        dif = newleft - newright;
+
+        if strcmp(mode,'int') |strcmp(mode,'int2')
+            dif2 = zeros(1,l+2*stretch);
+            dif2(j:j-1+idxright) = dif(j:j-1+idxright);     %only the errors before we cut the signal are important
+            %dif2(find(abs(newright)>0.05*maxright)) = dif(find(abs(newright)>0.05*maxright));  %only errors where the right signal is not zero are important
+
+            dif=dif2;
+            dif(j:j-1+idxright)=weight.*dif(j:j-1+idxright);    %here we say which errors are more important than others
+        end
+
+        %dif = dif.*weight*100;
+        %dif = dif.^2;
+        dif=abs(dif);
+        sumdif = sum(dif);
+
+
+        if ((sumdif < difmin) & abs(((-stretch+j)/fs)-itdold)<fit   ) | j==1
+            jmin = j;
+            difmin=sumdif;
+            difi=dif;
+            itd(ii) = (-stretch+j)/fs;
+        end
+    end
+    itdold=itd(ii);
+    jminold=jmin;
+
+    if 0 %mod(ii,30)==0                                                                 %plots the signals for the given angels
+        newright = zeros(1,l+2*stretch);
+        if strcmp(mode,'int3')
+            newright(jmin:jmin-1+l) = insigright(:,ii);
+        else
+            newright(jmin:jmin-1+idxright) = insigright(1:idxright,ii); %we cut the right(shifting) signal after the maximum
+        end
+        range = 200:1000;
+        plot(range,newright(range),range,newleft(range),range,difi(range),jmin:jmin-1+idxright,weight*0.0000002);
+        grid on;
+        file = sprintf('results/itd_%s_%i.png',mode,ii);
+        print(file);
+    end
 
 end
 end

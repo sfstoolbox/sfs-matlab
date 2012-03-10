a51501ff9557f3b7e170d3ccec4a01aacb8468d1
wierstorf.hagen@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2012-03-10 17:35:36 +0000

    Fixed a bug in delayline
    I have renamed s to sig
    
    --HG--
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40673

--- SFS_general/delayline.m
+++ SFS_general/delayline.m
@@ -1,7 +1,7 @@
 function sig = delayline(sig,dt,weight,conf)
 %DELAYLINE implements a (fractional) weighting delay line
-%   Usage: s = delayline(sig,dt,weight,conf)
-%          s = delayline(sig,dt,weight)
+%   Usage: sig = delayline(sig,dt,weight,conf)
+%          sig = delayline(sig,dt,weight)
 %          
 %   Input parameter:
 %       sig     - input signal (vector)
@@ -45,27 +45,27 @@ if(usefracdelay)
 
     switch fracdelay_method
     case 'resample'
-       s2 = resample(s,rfactor,1);
-       s2 = delayline(s2,rfactor*dt,weight,conf2);
-       s = resample(s2,1,rfactor);
+       sig2 = resample(sig,rfactor,1);
+       sig2 = delayline(sig2,rfactor*dt,weight,conf2);
+       sig = resample(sig2,1,rfactor);
 
     case 'least_squares'
         idt=floor(dt);
-        s = delayline(s,idt,weight,conf2);
+        sig = delayline(sig,idt,weight,conf2);
         if(abs(dt-idt)>0)
             % FIXME: add this function to Toolbox, or display URL for download
             h = hgls2(Lls,dt-idt,0.90);
             %[IP,wprot] = iniheq2(Lls,0.90);
             %h = heqrip2(Lls,dt-idt,wprot,IP);
-            s = conv(s,h);
-            s = s(Lls/2:end-Lls/2);
+            sig = conv(sig,h);
+            sig = sig(Lls/2:end-Lls/2);
         end
 
     case 'interp1'
         idt=floor(dt);
-        s = delayline(s,idt,weight,conf2);
-        t=1:length(s);
-        s = interp1(t,s,-(dt-idt)+t,'spline');
+        sig = delayline(sig,idt,weight,conf2);
+        t=1:length(sig);
+        sig = interp1(t,sig,-(dt-idt)+t,'spline');
         
     otherwise
         disp('Delayline: Unknown fractional delay method');
@@ -76,9 +76,8 @@ else
     idt=round(dt);
     % handle positive or negative delays
     if idt>=0
-        s = [zeros(1,idt) weight*s(1:end-idt)];
+        sig = [zeros(1,idt) weight*sig(1:end-idt)];
     else
-        s = [weight*s(-idt+1:end) zeros(1,-idt)];
+        sig = [weight*sig(-idt+1:end) zeros(1,-idt)];
     end
 end
-

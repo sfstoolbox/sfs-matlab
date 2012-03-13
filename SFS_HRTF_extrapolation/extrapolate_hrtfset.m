6e55340241e1d9ba22b8972c3a7c8f04a5c4bdc7
spors.sascha@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2012-03-13 13:00:14 +0000

    --HG-- extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40682

--- SFS_HRTF_extrapolation/extrapolate_hrtfset.m
+++ SFS_HRTF_extrapolation/extrapolate_hrtfset.m
@@ -1,13 +1,11 @@
-function brs = brsset_wfs_25d(X,phi,xs,L,src,irs,conf)
-%BRS_SET_WFS_25D generates a BRS set for use with the SoundScapeRenderer
-%   Usage: brs = brsset_wfs_25d(X,phi,xs,L,src,irs,conf)
-%          brs = brsset_wfs_25d(X,phi,xs,L,src,irs)
+function brs = extrapolate_hrtfset(phi,xs,src,irs,conf)
+%EXTRAPOLATE_HRTFSET extrapolates a given HRTF dataset
+%   Usage: brs = extrapolate_hrtfset(phi,xs,L,src,irs,conf)
+%          brs = extrapolate_hrtfset(phi,xs,L,src,irs)
 %
 %   Input parameters:
-%       X       - listener position (m)
 %       phi     - listener direction [head orientation] (rad)
 %       xs      - virtual source position [ys > Y0 => focused source] (m)
-%       L       - Length of linear loudspeaker array (m)
 %       src     - source type: 'pw' - plane wave
 %                              'ps' - point source
 %                              'fs' - focused source
@@ -19,43 +17,20 @@ function brs = brsset_wfs_25d(X,phi,xs,L,src,irs,conf)
 %       brs     - conf.N x 2*nangles matrix containing all brs (2
 %                 channels) for every angles of the BRS set
 %
-%   BRSSET_WFS_25D(X,phi,xs,L,irs,conf) prepares a BRS set for
-%   a virtual source at xs for a linear WFS array and the given
-%   listener position.
-%   One way to use this BRS set is using the SoundScapeRenderer (SSR), see
-%   http://www.tu-berlin.de/?id=ssr
-%
-%   Geometry:
-%                               y-axis
-%                                 ^
-%                                 |
-%                                 |
-%                                 |    (Listener)
-%                                 |        O X, phi=-pi/2
-%                                 |        |
-%                                 |
-%                  o xs           |
-%             (Virtual Source)    |
-%                                 |
-%     -------v--v--v--v--v--v--v--v--v--v--v--v--v--v--v------> x-axis
-%                                 X0 (Array center)
-%            |---      Loudspeaker array length     ---|
-%
-%   see also: SFS_config, brs_wfs_25d, brs_point_source
+% FIXME: this works only for plane waves (xs/src is ignored)
 
-% AUTHOR: Sascha Spors, Hagen Wierstorf
-% $LastChangedDate: 2012-03-06 17:17:20 +0100 (Di, 06 Mrz 2012) $
-% $LastChangedRevision: 661 $
-% $LastChangedBy: spors.sascha $
+% AUTHOR: Sascha Spors
+% $LastChangedDate$
+% $LastChangedRevision$
+% $LastChangedBy$
 
 
 %% ===== Checking of input  parameters ==================================
-nargmin = 6;
-nargmax = 7;
+nargmin = 4;
+nargmax = 5;
 error(nargchk(nargmin,nargmax,nargin));
-[X,xs] = position_vector(X,xs);
+[xs] = position_vector(xs);
 isargscalar(phi);
-isargpositivescalar(L);
 check_irs(irs);
 
 if nargin<nargmax
@@ -68,15 +43,79 @@ end
 %% ===== Configuration ===================================================
 N = conf.N;                     % Target length of BRIR impulse responses
 angles = rad(conf.brsangles);   % Angles for the BRIRs
+fs = conf.fs;                   % sampling frequency
+
+%% ===== Variables ======================================================
+
+phi = correct_azimuth(phi);
+R = irs.distance;
+L = 2*R;
+nls = length(irs.apparent_azimuth);
+lenir = length(irs.left(:,1));
+
+% get virtual loudspeaker positions from HRTF dataset
+conf.array = 'circle';
+conf.dx0=2*pi*R/nls;
+conf.xref = [0 0 0];
+conf.x0=zeros(nls,6);
+conf.x0(:,1:2)=[R*cos(irs.apparent_azimuth) ; R*sin(irs.apparent_azimuth)]';
+
+x0 = secondary_source_positions(L,conf);
+
+brs = zeros(N,2*length(angles));
+
+Acorr=-1.7;                     % DAGA 2011 R=0.5m -> pw
+Af=Acorr*sin(angles);
+
+
+% append zeros or truncate IRs to target length
+if(lenir<N)
+    irs.left=cat(1,ir.left,zeros(N-lenir,2));
+    irs.right=cat(1,ir.right,zeros(N-lenir,2));
+else
+    irs.left=irs.left(1:N,:);
+    irs.right=irs.right(1:N,:);
+end
 
 
 %% ===== Computation =====================================================
-% Initial values
-brs = zeros(N,2*length(angles));
-
 % Generate a BRS set for all given angles
 for ii = 1:length(angles)
-    % Compute BRIR for the desired WFS system
-    brs(:,(ii-1)*2+1:ii*2) = ...
-        brs_wfs_25d(X,angles(ii)+phi,xs,L,src,irs,conf);
+    
+    % variables
+    brir = zeros(N,2);
+    % FIXME: this works only for plane waves (xs/src is ignored)
+    xs = -[cos(angles(ii)) sin(angles(ii))];
+    
+    % calculate active virtual speakers
+    ls_activity = secondary_source_selection(x0,xs,src);
+    
+    % generate tapering window
+    win = tapwin(L,ls_activity,conf);
+    
+    % sum up contributions from individual virtual speakers
+    aidx=find(ls_activity>0);
+    for l=aidx'
+        % Driving function to get weighting and delaying
+        [a,delay] = driving_function_imp_wfs_25d(x0(l,:),xs,src,conf);
+        dt = delay*fs + R/conf.c*fs;
+        w=a*win(l);
+        
+        % delay and weight HRTFs
+        brir(:,1) = brir(:,1) + delayline(irs.left(:,l)',dt,w,conf)';
+        brir(:,2) = brir(:,2) + delayline(irs.right(:,l)',dt,w,conf)';
+    end
+    
+    brir(:,1)=brir(:,1)*10^(Af(ii)/20);
+    brir(:,2)=brir(:,2)*10^(-Af(ii)/20);
+    
+    % store result to output variable
+    brs(:,(ii-1)*2+1:ii*2) = brir;
+    %plot(brir); pause(0.1);
 end
+
+%% ===== Pre-equalization ===============================================
+brs = wfs_preequalization(brs,conf);
+
+%% ===== Headphone compensation =========================================
+brs = compensate_headphone(brs,conf);

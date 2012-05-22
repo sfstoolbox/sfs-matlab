c88e693998b1fe77f76a1d396a2be1851ab9a779
spors.sascha@e730da3b-c3e2-4fc5-b1ca-054ac94edddb <none@none>
2012-05-22 11:55:39 +0000

    Renamed SDM driving functions
    --HG--
    rename : SFS_monochromatic/driving_function_mono_sdm_25d_sd.m => SFS_monochromatic/driving_function_mono_sdm_25d.m
    rename : SFS_monochromatic/driving_function_mono_sdm_25d.m => SFS_monochromatic/driving_function_mono_sdm_25d_kx.m
    extra : convert_revision : svn%3Ae730da3b-c3e2-4fc5-b1ca-054ac94edddb/trunk%40735

--- SFS_monochromatic/driving_function_mono_sdm_25d.m
+++ SFS_monochromatic/driving_function_mono_sdm_25d.m
@@ -1,11 +1,11 @@
-function [Dkx] = driving_function_mono_sdm_25d(kx,xs,f,src,conf)
-%DRIVING_FUNCTION_MONO_SDM_25D returns the driving signal D for 2.5D SDM
+function [D] = driving_function_mono_sdm_25d_sd(x0,xs,f,src,conf)
+%DRIVING_FUNCTION_MONO_SDM_25D_SD returns the driving signal D for 2.5D SDM
+%IN THE SPACIAL DOMAIN
 %
-%   Usage: Dkx = driving_function_mono_sdm_25d(kx,xs,f,src,[conf])
+%   Usage: D = driving_function_mono_sdm_25d_sd(x0,xs,f,src,[conf])
 %
 %   Input parameters:
-%       kx          - position of the secondary source in the spectro-temporal
-%                     domain (m)
+%       x           - positions of the secondary sources (m)
 %       xs          - position of virtual source or direction of plane wave (m)
 %       f           - frequency of the monochromatic source (Hz)
 %       src         - source type of the virtual source
@@ -16,11 +16,12 @@ function [Dkx] = driving_function_mono_sdm_25d(kx,xs,f,src,conf)
 %       conf        - optional configuration struct (see SFS_config)
 %
 %   Output parameters:
-%       Dkx         - driving function signal (n x n)
+%       D         - driving function
 %
-%   DRIVING_FUNCTION_MONO_SDM_25D(x0,xs,f,src,conf) returns the
-%   driving signal for the given secondary source and desired source type (src).
-%   The driving signal is calculated for the SDM 2.5 dimensional case.
+%   DRIVING_FUNCTION_MONO_SDM_25D_SD(x0,xs,f,src,conf) returns the
+%   space-domain driving signal for the given secondary sources and 
+%   desired source type (src). The driving signal is calculated for the 
+%   SDM 2.5 dimensional case.
 
 %*****************************************************************************
 % Copyright (c) 2010-2012 Quality & Usability Lab                            *
@@ -49,7 +50,7 @@ function [Dkx] = driving_function_mono_sdm_25d(kx,xs,f,src,conf)
 % http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
 %*****************************************************************************
 
-% AUTHOR: Hagen Wierstorf
+% AUTHOR: Sascha Spors
 % $LastChangedDate$
 % $LastChangedRevision$
 % $LastChangedBy$
@@ -59,7 +60,7 @@ function [Dkx] = driving_function_mono_sdm_25d(kx,xs,f,src,conf)
 nargmin = 4;
 nargmax = 5;
 error(nargchk(nargmin,nargmax,nargin));
-isargmatrix(kx);
+isargsecondarysource(x0);
 isargposition(xs);
 xs = position_vector(xs);
 isargpositivescalar(f);
@@ -76,43 +77,35 @@ phase = conf.phase;
 xref = position_vector(conf.xref);
 X0 = conf.X0;
 c = conf.c;
-withev = conf.withev;
 
 
 %% ===== Variables =======================================================
 % Omega
 omega = 2*pi*f;
-% Indexes for evanescent contributions and propagating part of the wave field
-idxpr = (( abs(kx) <= (omega/c) ));
-idxev = (( abs(kx) > (omega/c) ));
+% positions and angles of secondary sources
+x0 = x0(:,1:3);
+al=atan2(x0(:,2),x0(:,1));
+% number of secondary sources
+M=length(x0);
+% virtual source angle
+al_pw=atan2(xs(2),xs(1));
+
 
 
 %% ===== Spectrum of driving function ====================================
-Dkx = zeros(1,length(kx));
+D = zeros(1,length(x0));
 
 if strcmp('pw',src)
 
     % ===== PLANE WAVE ===================================================
-    al_pw=atan2(xs(2),xs(1));
-    kpwx=omega/c*cos(al_pw);
-    kpwy=omega/c*sin(al_pw);
-    idx = find(kx>=kpwx,1,'first');
-    
-    Dkx(idx) = 4i*exp(-1i*kpwy*xref(2))./besselh(0,2,kpwy*xref(2));
-    
-    % spectral repetitions
-    %     Nrep=6;
-    %     kxrep=2*pi/0.2;
-    %     if(Nrep>0)
-    %         for n=-Nrep:Nrep
-    %             if(n~=0)
-    %                 kxp=kpwx-n*kxrep;
-    %     
-    %                 idx=find(kx>=kxp,1,'first');
-    %                     Dkx(idx) = 4i*exp(-1i*kpwy*xref(2))./besselh(0,2,kpwy*xref(2));
-    %             end
-    %         end
-    %     end
+    D=zeros(1,length(x0));
+
+    kx=omega/c*cos(al_pw);
+    ky=omega/c*sin(al_pw);
+
+    for n=1:length(x0)
+        D(n) = 4*1i*exp(-1i*ky*xref(2))./besselh(0,2,ky*xref(2)).*exp(-1i*(kx*x0(n,1)+ky*x0(n,2)));
+    end
     
 
 elseif strcmp('ps',src)
@@ -123,32 +116,7 @@ elseif strcmp('ps',src)
 elseif strcmp('fs',src)
 
     % ===== FOCUSED SOURCE ===============================================
-    %
-    % Exact driving function for 25D synthesis
-    %
-    % D_25D(kx,w) = e^(i kx xs) ...
-    %                                   ____________
-    %                         H0^(2)( \|(w/c)^2-kx^2 |yref-ys| )
-    %                     / - --------------_-_-_-_-_-_---------, |kx|<|w/c|
-    %                     |      H0^(2)( \|(w/c)^2-kx^2 yref ) 
-    %                    <        ____________
-    %                     | K0( \|kx^2-(w/c)^2 |yref-ys| )
-    %                     \ ----------_-_-_-_-_-_---------,       |kx|>|w/c|
-    %                          K0( \|kx^2-(w/c)^2 yref )
-    %
-    % NOTE: the phase term e^(i phase) is only there in order to be able
-    %       to simulate different time steps
-    %
-    Dkx(idxpr) =  exp(1i*kx(idxpr)*xs(1)) .* ...
-        besselh(0,2,sqrt( (omega/c)^2 - kx(idxpr).^2 )*abs(xref(2)-xs(2))) ./ ...
-        besselh(0,2,sqrt( (omega/c)^2 - kx(idxpr).^2 )*abs(xref(2)-X0(2))) .* ...
-        exp(1i*phase);
-    if(withev)
-        Dkx(idxev) =  exp(1i*kx(idxev)*xs(1)) .* ...
-            besselk(0,sqrt(kx(idxev).^2 - (omega/c).^2)*abs(xref(2)-xs(2))) ./ ...
-            besselk(0,sqrt(kx(idxev).^2 - (omega/c).^2)*abs(xref(2)-X0(2))) .* ...
-            exp(1i*phase);
-    end
+    to_be_implemented(mfilename);
 
 else
     % No such source type for the driving function
--- SFS_monochromatic/driving_function_mono_sdm_25d_kx.m
+++ SFS_monochromatic/driving_function_mono_sdm_25d_kx.m
@@ -1,11 +1,11 @@
-function [D] = driving_function_mono_sdm_25d_sd(x0,xs,f,src,conf)
-%DRIVING_FUNCTION_MONO_SDM_25D_SD returns the driving signal D for 2.5D SDM
-%IN THE SPACIAL DOMAIN
-%
-%   Usage: D = driving_function_mono_sdm_25d_sd(x0,xs,f,src,[conf])
+function [Dkx] = driving_function_mono_sdm_25d_kx(kx,xs,f,src,conf)
+%DRIVING_FUNCTION_MONO_SDM_25D_KX returns the driving signal D for 2.5D SDM
+%IN THE SPATIAL FREQUENCY (kx-)DOMAIN
+%   Usage: Dkx = driving_function_mono_sdm_25d_kx(kx,xs,f,src,[conf])
 %
 %   Input parameters:
-%       x           - positions of the secondary sources (m)
+%       kx          - position of the secondary source in the spectro-temporal
+%                     domain (m)
 %       xs          - position of virtual source or direction of plane wave (m)
 %       f           - frequency of the monochromatic source (Hz)
 %       src         - source type of the virtual source
@@ -16,12 +16,12 @@ function [D] = driving_function_mono_sdm_25d_sd(x0,xs,f,src,conf)
 %       conf        - optional configuration struct (see SFS_config)
 %
 %   Output parameters:
-%       D         - driving function
+%       Dkx         - driving function signal (n x n)
 %
-%   DRIVING_FUNCTION_MONO_SDM_25D_SD(x0,xs,f,src,conf) returns the
-%   space-domain driving signal for the given secondary sources and 
-%   desired source type (src). The driving signal is calculated for the 
-%   SDM 2.5 dimensional case.
+%   DRIVING_FUNCTION_MONO_SDM_25D_KX(x0,xs,f,src,conf) returns the
+%   driving signal for the given secondary source and desired source type (src).
+%   The driving signal is calculated for the SDM 2.5 dimensional case in the
+%   spatial frequency domain.
 
 %*****************************************************************************
 % Copyright (c) 2010-2012 Quality & Usability Lab                            *
@@ -50,7 +50,7 @@ function [D] = driving_function_mono_sdm_25d_sd(x0,xs,f,src,conf)
 % http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
 %*****************************************************************************
 
-% AUTHOR: Sascha Spors
+% AUTHOR: Hagen Wierstorf
 % $LastChangedDate$
 % $LastChangedRevision$
 % $LastChangedBy$
@@ -60,7 +60,7 @@ function [D] = driving_function_mono_sdm_25d_sd(x0,xs,f,src,conf)
 nargmin = 4;
 nargmax = 5;
 error(nargchk(nargmin,nargmax,nargin));
-isargsecondarysource(x0);
+isargmatrix(kx);
 isargposition(xs);
 xs = position_vector(xs);
 isargpositivescalar(f);
@@ -77,35 +77,43 @@ phase = conf.phase;
 xref = position_vector(conf.xref);
 X0 = conf.X0;
 c = conf.c;
+withev = conf.withev;
 
 
 %% ===== Variables =======================================================
 % Omega
 omega = 2*pi*f;
-% positions and angles of secondary sources
-x0 = x0(:,1:3);
-al=atan2(x0(:,2),x0(:,1));
-% number of secondary sources
-M=length(x0);
-% virtual source angle
-al_pw=atan2(xs(2),xs(1));
-
+% Indexes for evanescent contributions and propagating part of the wave field
+idxpr = (( abs(kx) <= (omega/c) ));
+idxev = (( abs(kx) > (omega/c) ));
 
 
 %% ===== Spectrum of driving function ====================================
-D = zeros(1,length(x0));
+Dkx = zeros(1,length(kx));
 
 if strcmp('pw',src)
 
     % ===== PLANE WAVE ===================================================
-    D=zeros(1,length(x0));
-
-    kx=omega/c*cos(al_pw);
-    ky=omega/c*sin(al_pw);
-
-    for n=1:length(x0)
-        D(n) = 4*1i*exp(-1i*ky*xref(2))./besselh(0,2,ky*xref(2)).*exp(-1i*(kx*x0(n,1)+ky*x0(n,2)));
-    end
+    al_pw=atan2(xs(2),xs(1));
+    kpwx=omega/c*cos(al_pw);
+    kpwy=omega/c*sin(al_pw);
+    idx = find(kx>=kpwx,1,'first');
+    
+    Dkx(idx) = 4i*exp(-1i*kpwy*xref(2))./besselh(0,2,kpwy*xref(2));
+    
+    % spectral repetitions
+    %     Nrep=6;
+    %     kxrep=2*pi/0.2;
+    %     if(Nrep>0)
+    %         for n=-Nrep:Nrep
+    %             if(n~=0)
+    %                 kxp=kpwx-n*kxrep;
+    %     
+    %                 idx=find(kx>=kxp,1,'first');
+    %                     Dkx(idx) = 4i*exp(-1i*kpwy*xref(2))./besselh(0,2,kpwy*xref(2));
+    %             end
+    %         end
+    %     end
     
 
 elseif strcmp('ps',src)
@@ -116,7 +124,32 @@ elseif strcmp('ps',src)
 elseif strcmp('fs',src)
 
     % ===== FOCUSED SOURCE ===============================================
-    to_be_implemented(mfilename);
+    %
+    % Exact driving function for 25D synthesis
+    %
+    % D_25D(kx,w) = e^(i kx xs) ...
+    %                                   ____________
+    %                         H0^(2)( \|(w/c)^2-kx^2 |yref-ys| )
+    %                     / - --------------_-_-_-_-_-_---------, |kx|<|w/c|
+    %                     |      H0^(2)( \|(w/c)^2-kx^2 yref ) 
+    %                    <        ____________
+    %                     | K0( \|kx^2-(w/c)^2 |yref-ys| )
+    %                     \ ----------_-_-_-_-_-_---------,       |kx|>|w/c|
+    %                          K0( \|kx^2-(w/c)^2 yref )
+    %
+    % NOTE: the phase term e^(i phase) is only there in order to be able
+    %       to simulate different time steps
+    %
+    Dkx(idxpr) =  exp(1i*kx(idxpr)*xs(1)) .* ...
+        besselh(0,2,sqrt( (omega/c)^2 - kx(idxpr).^2 )*abs(xref(2)-xs(2))) ./ ...
+        besselh(0,2,sqrt( (omega/c)^2 - kx(idxpr).^2 )*abs(xref(2)-X0(2))) .* ...
+        exp(1i*phase);
+    if(withev)
+        Dkx(idxev) =  exp(1i*kx(idxev)*xs(1)) .* ...
+            besselk(0,sqrt(kx(idxev).^2 - (omega/c).^2)*abs(xref(2)-xs(2))) ./ ...
+            besselk(0,sqrt(kx(idxev).^2 - (omega/c).^2)*abs(xref(2)-X0(2))) .* ...
+            exp(1i*phase);
+    end
 
 else
     % No such source type for the driving function

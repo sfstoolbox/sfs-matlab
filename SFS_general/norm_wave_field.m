6d9ba07cd010559220978aa9ca0bfad0297f6d79
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-02-27 09:33:43 +0100

    Make norm_wave_field work with listening areas that are sampled with delta>0.1

--- SFS_general/norm_wave_field.m
+++ SFS_general/norm_wave_field.m
@@ -59,16 +59,20 @@ end
 
 %% ===== Configuration ===================================================
 xref = position_vector(conf.xref);
+xysamples = conf.xysamples;
 
 
 %% ===== Computation =====================================================
 % Use the half of the x axis and xref
 [a,xidx] = find(x>xref(1),1);
 [a,yidx] = find(y>xref(2),1);
-if isempty(xidx) || abs(x(xidx)-xref(1))>0.1
+% abs(x(1)-x(end))/xysamples gives us the maximum distance between to samples.
+% If abs(x(xidx)-xref(1)) is greater this indicates that we are out of our
+% bounds
+if isempty(xidx) || abs(x(xidx)-xref(1))>2*abs(x(1)-x(end))/xysamples
     error('%s: your used conf.xref is out of your X boundaries',upper(mfilename));
 end
-if isempty(yidx) || abs(y(yidx)-xref(2))>0.1
+if isempty(yidx) || abs(y(yidx)-xref(2))>2*abs(y(1)-y(end))/xysamples
     error('%s: your used conf.xref is out of your Y boundaries',upper(mfilename));
 end
 % Scale signal to 1

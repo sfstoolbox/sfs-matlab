f952b4a96329d669ac88423e5a5fd6c8c9bdfcce
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-03-08 17:50:37 +0100

    Added some thoughts to the generic handling of IR

--- SFS_binaural_synthesis/ir_generic.m
+++ SFS_binaural_synthesis/ir_generic.m
@@ -109,6 +109,9 @@ for ii=1:size(x0,1)
     % Angle between listener and secondary source (-pi < alpha <= pi)
     % Note: phi is the orientation of the listener (see first graph)
     [alpha,theta_tmp,r_tmp] = cart2sph(x0(ii,1)-X(1),x0(ii,2)-X(2),0);
+    
+    % direction of the source from the listener
+    x_direction = x0-X;
     %
     % Ensure -pi <= alpha < pi
     alpha = correct_azimuth(alpha-phi);

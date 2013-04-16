49f8e96131bf66039fb2c4b963491a801d49c5e2
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-04-16 17:29:44 +0200

    Added a common WFS driving function file

---
+++ SFS_monochromatic/driving_function_mono_wfs.m
@@ -0,0 +1,127 @@
+function D = driving_function_mono_wfs(x0,xs,src,f,conf)
+%DRIVING_FUNCTION_MONO_WFS returns the driving signal D for WFS
+%
+%   Usage: D = driving_function_mono_wfs(x0,xs,src,f,[conf])
+%
+%   Input parameters:
+%       x0          - position and direction of the secondary source (m) [nx6]
+%       xs          - position of virtual source or direction of plane wave (m)
+%       src         - source type of the virtual source
+%                         'pw' - plane wave (xs is the direction of the
+%                                plane wave in this case)
+%                         'ps' - point source
+%                         'fs' - focused source
+%       f           - frequency of the monochromatic source (Hz)
+%       conf        - optional configuration struct (see SFS_config)
+%
+%   Output parameters:
+%       D           - driving function signal [nx1]
+%
+%   DRIVING_FUNCTION_MONO_WFS(x0,xs,f,src,conf) returns the driving signal for
+%   the given secondary source and desired source type (src) for WFS for the
+%   given frequency.
+%
+%   References:
+%       Spors2009 - Physical and Perceptual Properties of Focused Sources in
+%           Wave Field Synthesis (AES127)
+%       Spors2010 - Analysis and Improvement of Pre-equalization in
+%           2.5-Dimensional Wave Field Synthesis (AES128)
+%       Williams1999 - Fourier Acoustics (Academic Press)
+%
+%   see also: plot_wavefield, wave_field_mono_wfs_25d,
+%             driving_function_imp_wfs_25d
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input  parameters ==================================
+nargmin = 4;
+nargmax = 5;
+narginchk(nargmin,nargmax);
+isargsecondarysource(x0);
+isargposition(xs);
+xs = position_vector(xs);
+isargpositivescalar(f);
+isargchar(src);
+if nargin<nargmax
+    conf = SFS_config;
+else
+    isargstruct(conf);
+end
+
+
+%% ===== Configuration ==================================================
+% phase of omega
+phase = conf.phase;
+
+
+%% ===== Computation ====================================================
+
+% Calculate the driving function in time-frequency domain
+
+% Secondary source positions and directions
+nx0 = x0(:,4:6);
+x0 = x0(:,1:3);
+
+% Source position
+xs = repmat(xs,[size(x0,1) 1]);
+
+% Get driving signals
+if strcmp('pw',src)
+    % === Plane wave =====================================================
+    % Direction of plane wave
+    nk = bsxfun(@rdivide,xs,vector_norm(xs,2));
+    % Driving signal
+    D = driving_function_mono_wfs_pw(x0,nx0,nk,f,conf);
+
+elseif strcmp('ps',src)
+    % === Point source ===================================================
+    % Driving Signal
+    D = driving_function_mono_wfs_ps(x0,nx0,xs,f,conf);
+
+elseif strcmp('ls',src)
+    % === Line source ====================================================
+    % Driving signal
+    D = driving_function_mono_wfs_ls(x0,nx0,xs,f,conf);
+
+elseif strcmp('fs',src)
+    % === Focused source =================================================
+    % Driving Signal
+    D = driving_function_mono_wfs_fs(x0,nx0,xs,f,conf);
+
+else
+    error('%s: %s is not a known source type.',upper(mfilename),src);
+end
+
+% Add phase to be able to simulate different time steps
+D = D .* exp(-1i*phase);

c7b8c875782779f250dd6a792b25466760191a1f
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-04-16 10:57:56 +0200

    Added a WFS driving function for plane waves

---
+++ SFS_monochromatic/driving_functions_mono/driving_function_mono_wfs_pw.m
@@ -0,0 +1,180 @@
+function D = driving_function_mono_wfs_pw(x0,nx0,nk,f,conf)
+%DRIVING_FUNCTION_MONO_WFS_PW returns the driving signal D for a plane wave in
+%WFS
+%
+%   Usage: D = driving_function_mono_wfs_pw(x0,nx0,nk,f,[conf])
+%
+%   Input parameters:
+%       x0          - position of the secondary sources (m) [nx3]
+%       nx0         - directions of the secondary sources (m) [nx3]
+%       nk          - direction of plane wave (m) [nx3]
+%       f           - frequency of the monochromatic source (Hz)
+%       conf        - optional configuration struct (see SFS_config)
+%
+%   Output parameters:
+%       D           - driving function signal [nx1]
+%
+%   DRIVING_FUNCTION_MONO_WFS_PW(x0,xs,f,src,conf) returns WFS driving signals
+%   for the given secondary sources, the virtual plane wave direction and the
+%   frequency f.
+%
+%   References:
+%       FIXME: Update references
+%       Spors2009 - Physical and Perceptual Properties of Focused Sources in
+%           Wave Field Synthesis (AES127)
+%       Spors2010 - Analysis and Improvement of Pre-equalization in
+%           2.5-Dimensional Wave Field Synthesis (AES128)
+%       Williams1999 - Fourier Acoustics (Academic Press)
+%
+%   see also: driving_function_mono_wfs, driving_function_imp_wfs_ps
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input  parameters ==================================
+nargmin = 4;
+nargmax = 5;
+narginchk(nargmin,nargmax);
+isargmatrix(x0,nx0,xs);
+isargpositivescalar(f);
+if nargin<nargmax
+    conf = SFS_config;
+else
+    isargstruct(conf);
+end
+
+
+%% ===== Configuration ==================================================
+xref = position_vector(conf.xref);
+c = conf.c;
+dimension = conf.dimension;
+driving_functions = conf.driving_functions;
+
+
+%% ===== Computation ====================================================
+% Calculate the driving function in time-frequency domain
+
+% frequency
+omega = 2*pi*f;
+
+if strcmp('2D',dimension)
+    
+    % === 2-Dimensional ==================================================
+    
+    % Ensure 2D
+    x0 = x0(:,1:2);
+    nx0 = nx0(:,1:2);
+    xs = xs(:,1:2);
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        % D_2D using a plane wave as source model
+        %
+        %                i w
+        % D_2D(x0,w) = 2 --- nk nx0  e^(-i w/c nk x0)
+        %                 c
+        %
+        D = 2*1i*omega/c .* vector_product(nk,nx0,2) .*  ...
+            exp(-1i*omega/c.*vector_product(nk,x0,2));
+        %
+    elseif strcmp('delft1988',driving_functions)
+        % --- Delft 1988 -------------------------------------------------
+        to_be_implemented;
+        %
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 2D point source.'],upper(mfilename),driving_functions);
+    end
+
+
+elseif strcmp('2.5D',dimension)
+    
+    % === 2.5-Dimensional ================================================
+    
+    % Reference point
+    xref = repmat(xref,[size(x0,1) 1]);
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        % 2.5D correction factor
+        %        ______________
+        % g0 = \| 2pi |xref-x0|
+        %
+        g0 = sqrt(2*pi*vector_norm(xref-x0,2));
+        %
+        % D_2.5D using a plane wave as source model
+        %                               ___
+        %                              | w |
+        % D_2.5D(x0,w) = 2 g0 nk nx0 _ |---  e^(-i w/c nk x0)
+        %                             \|i c
+        %
+        D = 2*g0 .* vector_product(nk,nx0,2) .* sqrt(omega/(1i*c)) .* ...
+            exp(-1i*omega/c.*vector_product(nxs,x0,2));
+        %
+    elseif strcmp('delft1988',driving_functions)
+        % --- Delft 1988 -------------------------------------------------
+        to_be_implemented;
+        %
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 2.5D point source.'],upper(mfilename),driving_functions);
+    end
+
+
+elseif strcmp('3D',dimension)
+    
+    % === 3-Dimensional ==================================================
+    
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        % D_3D using a plane wave as source model
+        % FIXME: in 2D case it is 2, here it is -2
+        %
+        %                  i w 
+        % D_3D(x0,w) =  -2 --- nk nx0  e^(-i w/c nk x0) .* weights
+        %                   c
+        %
+        D(ii) = -2*1i*omega/c .* vector_product(nk,nx0,2) .* ...
+            exp(-1i*omega/c.*vector_product(nk,x0,2)) .* ...
+                equallyPointsWeights .* surfaceWeights;
+        %
+    elseif strcmp('delft1988',driving_functions)
+        % --- Delft 1988 -------------------------------------------------
+        to_be_implemented;
+        %
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 3D point source.'],upper(mfilename),driving_functions);
+    end
+
+else
+    error('%s: the dimension %s is unknown.',upper(mfilename),dimension);
+end

d59841d904be344dd99703dd500224c1a2e5041f
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-04-16 21:19:30 +0200

    Added helper file to check for xs ([1x3] or [1x6])

---
+++ SFS_helper/isargxs.m
@@ -0,0 +1,55 @@
+function isargxs(varargin)
+%ISARGXS tests if the given args are a [1x3] and/or [1x6] vector and returns
+%an error otherwise
+%
+%   Usage: isargxs(args)
+%
+%   Input options:
+%       args        - list of args
+%
+%   ISARGXS(args) tests if all given args are a [1x3] and/or [1x6]  vectors and
+%   returns an error otherwise.
+%
+%   see also: isargscalar, isargvector, isargposition
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking for vector =============================================
+for ii = 1:nargin
+    if ~isvector(varargin{ii}) || ...
+            ( ~all(size(varargin{ii})==[1 3]) && ...
+        ~all(size(varargin{ii})==[1 6]) )
+        error('%s needs to be a [1x3] or [1x6] vector.',inputname(ii));
+    end
+end

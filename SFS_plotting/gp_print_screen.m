afe5c1435988f3040e0b1675d5562620ee7c833d
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-04 15:00:39 +0200

    Updated comments

--- SFS_plotting/gp_print_screen.m
+++ SFS_plotting/gp_print_screen.m
@@ -1,5 +1,49 @@
 function cmd = gp_print_screen(p)
+%GP_PRINT_SCREEN returns the gnuplot command to print to the screen
+%
+%   See also: plot_wavefield
 
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input parameters ====================================
+nargmin = 1;
+nargmax = 1;
+nargchkin(nargmin,nargmax);
+
+
+%% ===== Main ============================================================
 % get plotting command, including loudspeaker symbols and sound field
 cmd_plot = gp_get_plot_command(p);
 

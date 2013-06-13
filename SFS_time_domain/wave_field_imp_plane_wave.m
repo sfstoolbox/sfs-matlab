c4b6b572bb2144a9eb88df3b2b8d3a3645f4a71a
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-13 13:16:54 +0200

    Added imp functions for simple source models

---
+++ SFS_time_domain/wave_field_imp_plane_wave.m
@@ -0,0 +1,77 @@
+function varargout = wave_field_imp_plane_wave(X,Y,Z,xs,t,conf)
+%WAVE_FIELD_IMP_PLANE_WAVE simulates a wave field of a plane wave
+%
+%   Usage: [P,x,y,z] = wave_field_imp_plane_wave(X,Y,Z,xs,t,[conf])
+%
+%   Input parameters:
+%       X           - [xmin,xmax]
+%       Y           - [ymin,ymax]
+%       Z           - [zmin,zmax]
+%       xs          - direction of the plane wave
+%       t           - time (samples)
+%       conf        - optional configuration struct (see SFS_config)
+%
+%   Output parameters:
+%       P           - Simulated wave field
+%       x           - corresponding x axis
+%       y           - corresponding y axis
+%       z           - corresponding z axis
+%
+%   WAVE_FIELD_IMP_PLANE_WAVE(X,Y,Z,xs,f,conf) simulates a wave
+%   field of a plane wave going in the direction xs.
+%   To plot the result use plot_wavefield(P,x,y,z).
+%
+%   References:
+%       Williams1999 - Fourier Acoustics (Academic Press)
+%
+%   see also: wave_field_mono, plot_wavefield, wave_field_mono_plane_wave
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input  parameters ==================================
+nargmin = 5;
+nargmax = 6;
+narginchk(nargmin,nargmax);
+isargxs(xs);
+if nargin<nargmax
+    conf = SFS_config;
+else
+    isargstruct(conf);
+end
+
+
+%% ===== Computation ====================================================
+% Disable the plotting of a source, because we have a plane wave
+conf.plot.loudspeakers = 0;
+[varargout{1:nargout}] = wave_field_mono(X,Y,Z,[xs 0 1 0],'pw',1,t,conf);

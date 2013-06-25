a8d40a1b36a790f25990d45652072a12df2090a8
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-25 18:06:42 +0200

    Added dummy driving functions for line source for SDM and NFCHOA

---
+++ SFS_monochromatic/driving_functions_mono/driving_function_mono_nfchoa_ls.m
@@ -0,0 +1,123 @@
+function D = driving_function_mono_nfchoa_ls(x0,xs,f,conf)
+%DRIVING_FUNCTION_MONO_NFCHOA_LS returns the driving signal D for a line source
+%in NFCHOA
+%
+%   Usage: D = driving_function_mono_nfchoa_ls(x0,xs,f,[conf])
+%
+%   Input parameters:
+%       x0          - position of the secondary sources (m) [nx3]
+%       xs          - position of virtual line source (m) [nx3]
+%       f           - frequency of the monochromatic source (Hz)
+%       conf        - optional configuration struct (see SFS_config)
+%
+%   Output parameters:
+%       D           - driving function signal [nx1]
+%
+%   DRIVING_FUNCTION_MONO_NFCHOA_LS(x0,xs,f,conf) returns NFCHOA driving
+%   signals for the given secondary sources, the virtual line source position
+%   and the frequency f.
+%
+%   References:
+%       FIXME: Update references
+%
+%   see also: driving_function_mono_nfchoa, driving_function_imp_nfchoa_ls
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input  parameters ==================================
+nargmin = 3;
+nargmax = 4;
+narginchk(nargmin,nargmax);
+isargmatrix(x0,xs);
+isargpositivescalar(f);
+if nargin<nargmax
+    conf = SFS_config;
+else
+    isargstruct(conf);
+end
+
+
+%% ===== Configuration ==================================================
+xref = conf.xref;
+c = conf.c;
+dimension = conf.dimension;
+driving_functions = conf.driving_functions;
+
+
+%% ===== Computation ====================================================
+% Calculate the driving function in time-frequency domain
+
+if strcmp('2D',dimension)
+    
+    % === 2-Dimensional ==================================================
+    
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        to_be_implemented;
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 2D line source.'],upper(mfilename),driving_functions);
+    end
+
+
+elseif strcmp('2.5D',dimension)
+    
+    % === 2.5-Dimensional ================================================
+    
+    % Reference point
+    xref = repmat(xref,[size(x0,1) 1]);
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        to_be_implemented;
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 2.5D line source.'],upper(mfilename),driving_functions);
+    end
+
+
+elseif strcmp('3D',dimension)
+    
+    % === 3-Dimensional ==================================================
+    
+    if strcmp('default',driving_functions)
+        % --- SFS Toolbox ------------------------------------------------
+        to_be_implemented;
+    else
+        error(['%s: %s, this type of driving function is not implemented ', ...
+            'for a 3D line source.'],upper(mfilename),driving_functions);
+    end
+
+else
+    error('%s: the dimension %s is unknown.',upper(mfilename),dimension);
+end

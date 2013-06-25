7394937e02347751e96bcf454d0c7f76bfb70ad9
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-25 10:05:36 +0200

    Added automatic distance calculation to aliasing_frequency()

--- SFS_general/aliasing_frequency.m
+++ SFS_general/aliasing_frequency.m
@@ -1,7 +1,8 @@
-function fal = aliasing_frequency(x0,conf)
-%ALIASING_FREQUENCY returns the aliasing frequency
+function [fal,dx0] = aliasing_frequency(x0,conf)
+%ALIASING_FREQUENCY returns the aliasing frequency for the given secondary
+%sources
 %
-%   Usage: fal = aliasing_frequency(x0,[conf])
+%   Usage: [fal,dx0] = aliasing_frequency(x0,[conf])
 %
 %   Input options:
 %       x0      - secondary sources / m
@@ -9,9 +10,14 @@ function fal = aliasing_frequency(x0,conf)
 %
 %   Output options:
 %       fal     - aliasing frequency / Hz
+%       dx0     - mean distance between secondary sources / m
 %
 %   ALIASING_FREQUENCY(x0,conf) returns the aliasing frequency for the given
-%   secondary sources. The value is calculated after Spors (2009).
+%   secondary sources. First the mean distance dx0 between the secondary sources
+%   is calculated, afterwards the aliasing frequency is calculated after Spors
+%   (2009) as fal = c/(2*dx0).
+%   For a calculation that includes the dependency on the listener position have
+%   a look at Start (1997).
 %   
 %   References:
 %       S. Spors and J. Ahrens - Spatial sampling artifacts of wave field
@@ -20,7 +26,7 @@ function fal = aliasing_frequency(x0,conf)
 %       E. Start - Direct Sound Enhancement by Wave Field Synthesis. TU Delft,
 %       1997.
 %
-%   see also: wave_field_mono_wfs, secondary_source_positions
+%   See also: wave_field_mono_wfs, secondary_source_positions
 
 %*****************************************************************************
 % Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
@@ -53,16 +59,43 @@ function fal = aliasing_frequency(x0,conf)
 %                                                                            *
 % http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
 %*****************************************************************************
+%   see also: secondary_source_positions
+
+%*****************************************************************************
+% Copyright (c) 2010-2012 Quality & Usability Lab                            *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
 
 
 %% ===== Checking of input parameters ====================================
 nargmin = 1;
 nargmax = 2;
-narginchk(nargmin,nargmax);
+error(nargchk(nargmin,nargmax,nargin));
 if nargin==nargmax-1
     conf = SFS_config;
 end
-isargsecondarysource(x0);
 
 
 %% ===== Configuration ==================================================
@@ -70,43 +103,20 @@ c = conf.c;
 
 
 %% ===== Computation =====================================================
-% Estimate the average distance between the secondary sources.
-to_be_implemented;
-dx0 = get_mean_distance(x0);
+% calculate the distance to the nearest secondary source for all secondary
+% sources
+for ii=1:size(x0,1)
+    % first secondary source position
+    x01 = x0(ii,1:3);
+    % all other positions
+    x02 = [x0(1:ii-1,1:3); x0(ii+1:end,1:3)];
+    % get distance between x01 and all secondary sources within x02
+    dist = bsxfun(@minus,x02,x01);
+    dist = vector_norm(dist,2);
+    % get the smallest distance (which is the one to the next source)
+    dx0(ii) = min(dist);
+end
+% get the mean distance between all secondary sources
+dx0 = mean(dx0);
+% calculate aliasing frequency
 fal = c/(2*dx0);
-return
-
-%FIXME: the following code works not probably at the moment. But it is a first
-%step into the direction of calculating the aliasing frequency depending on the
-%listener and virtual source position.
-% The alias frequency depends on the plane waves that are present in the sound
-% field. And which plane waves can be present depends on the concrete angle
-% between the source/listener and the array of secondary sources. The following
-% code uses the Fraunhofer approximation in order to calculate the alias
-% frequency, see Start1997 p73, eq 5.17
-%
-% get secondary source positions and activity
-x0 = secondary_source_positions(L,conf);
-x0 = secondary_source_selection(x0,xs,src);
-win = tapering_window(x0,conf);
-% === get angles between array edges and virtual source position ===
-% orientation of loudspeaker array
-v_array = x0(1,1:3)-x0(end,1:3);
-[phi_array,theta_tmp,r_tmp] = cart2sph(v_array(1),v_array(2),v_array(3))-pi/2;
-% angle between array edges and virtual source position
-v1 = x0(1,1:3)-xs;
-v2 = x0(end,1:3)-xs;
-[phix01,theta_tmp,r_tmp] = cart2sph(v1(1),v1(2),v1(3))-phi_array;
-[phix02,theta_tmp,r_tmp] = cart2sph(v2(1),v2(2),v2(3))-phi_array;
-% angle between array edges and listener position
-v1 = x0(1,1:3)-X;
-v2 = x0(end,1:3)-X;
-[phiX1,theta_tmp,r_tmp] = cart2sph(v1(1),v1(2),v1(3))+phi_array;
-[phiX2,theta_tmp,r_tmp] = cart2sph(v2(1),v2(2),v2(3))+phi_array;
-% use only the smallest of these angles
-phi1 = min([abs(phix01) abs(phiX1)]);
-phi2 = min([abs(phix02) abs(phiX2)]);
-%                    c
-% fal = -----------------------------
-%       dx0 (sin(|phi1|)+sin(|phi2|))
-fal = c / (dx0*(sin(phi1)+sin(phi2)));

afd72bfd98f0c2de336bdbff62ef433b8e48d97b
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-06-26 12:47:01 +0200

    Added dirac_imp() to generate stimulus for imp wave fields

---
+++ SFS_time_domain/dirac_imp.m
@@ -0,0 +1,53 @@
+function pulse = dirac_imp()
+%DIRAC_IMP retruns a pulse as stimulus for the imp driving functions
+%
+%   Usage: pulse = dirac_imp()
+%
+%   Output parameters:
+%       pulse    - short pulse signal
+%
+%   DIRAC_IMP() returns a 10 sample slong hann shaped pulse.
+%
+%   see also: hanningwin, driving_function_imp_wfs, driving_function_imp_nfchoa
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking input parameters =======================================
+nargmin = 0;
+nargmax = 0;
+narginchk(nargmin,nargmax);
+
+
+%% ===== Computation =====================================================
+pulse = row_vector(hanningwin(5,5,10));

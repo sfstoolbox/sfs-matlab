39ff18d8df55372157b49752a5f032f4013a0be8
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-07-02 11:17:22 +0200

    Added a function to get spherical grid points and weihgts

---
+++ SFS_general/get_spherical_grid.m
@@ -0,0 +1,103 @@
+function [points,weights] = get_spherical_grid(number,conf)
+%GET_SPHERICAL_GRID retruns grid points and weights
+%
+%   Usage: [points,weights] = get_spherical_grid(number,[conf])
+%
+%   Input parameters:
+%       number  - number of grid points
+%       conf    - optional configuration struct (see SFS_config)
+%
+%   Output parameters:
+%       points  - grid points
+%       weights - integration weights for the grid points
+%
+%   GET_SPHERICAL_GRID(number) returns the points and weights for a grid on a
+%   sphere. The type of grid is specified by conf.secondary_sources.grid.
+%   For available grids, have a look at http://github.com/sfstoolbox/data.
+%   It expects the grid files at SFS_basepath/data/spherical_grids. If the
+%   desired file is not available on the hard disk, the function tries to
+%   download it directly from github.
+%
+%   see also: secondary_source_positions,
+%       weights_for_points_on_a_sphere_rectangle
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking input parameters =======================================
+nargmin = 1;
+nargmax = 2;
+narginchk(nargmin,nargmax);
+isargpositivescalar(number);
+if nargin<nargmax
+    conf = SFS_config;
+else
+    isargstruct(conf);
+end
+
+
+%% ===== Configuration ===================================================
+spherical_grid = conf.secondary_sources.grid;
+
+
+%% ===== Main ============================================================
+filename = sprintf('%06.0fpoints.mat',number);
+basepath = get_sfs_path();
+
+if strcmp('equally_spaced_points',spherical_grid)
+    % check if we have a squared number of points, because only for those values
+    % are equally spaced points grids available.
+    if mod(number,sqrt(number))~=0
+        error('%s: number has to be a squared number.',upper(mfilename));
+    end
+    file = [basepath '/data/spherical_grids/equally_spaced_points/' filename];
+    url = ['http://github.com/sfstoolbox/data/raw/master/spherical_grids/' ...
+        'equally_spaced_points/' filename];
+    % download file if not present
+    if ~exist(file,'file')
+        % create dir
+        mkdir([basepath '/data']);
+        mkdir([basepath '/data/spherical_grids']);
+        mkdir([basepath '/data/spherical_grids/equally_spaced_points']);
+        % download file
+        warning('Downloading grid file from http://github.com/sfstoolbox/data');
+        urlwrite(url,file);
+    end
+    tmp = load(file,'-ascii');
+    points = tmp(:,1:3);
+    weights = tmp(:,4);
+else
+    error(['%s: the given spherical grid is not available, have a look at ' ...
+        'http://github.com/sfstoolbox/data for avialable grids.'], ...
+        upper(mfilename));
+end

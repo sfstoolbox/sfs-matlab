186aff28a10621316f3f10fee7560e2ffee8a1a4
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2013-07-05 17:05:07 +0200

    Updated license statements

--- SFS_general/weights_for_points_on_a_sphere_rectangle.m
+++ SFS_general/weights_for_points_on_a_sphere_rectangle.m
@@ -17,9 +17,14 @@ function [w] = weights_for_points_on_a_sphere_rectangle(phi,theta)
 %   (phi), elevation (theta) angle.
  
 %*****************************************************************************
-% Copyright (c) 2010-2012 Quality & Usability Lab                            *
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
 %                         Deutsche Telekom Laboratories, TU Berlin           *
 %                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
 %                                                                            *
 % This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
 %                                                                            *
--- SFS_ir/conversion/fabian3D2mat.m
+++ SFS_ir/conversion/fabian3D2mat.m
@@ -14,8 +14,38 @@ function fabian3D2mat(irsset)
 %   FABIAN3D2mat(irsset) converts the IRs data given by the irsset
 %   in our own mat-file based format. See:
 %   https://dev.qu.tu-berlin.de/projects/sfs/wiki/IRs_mat-file_format
-%
-% AUTHOR: Vincent Kuscher
+
+%*****************************************************************************
+% Copyright (c) 2010-2013 Quality & Usability Lab, together with             *
+%                         Assessment of IP-based Applications                *
+%                         Deutsche Telekom Laboratories, TU Berlin           *
+%                         Ernst-Reuter-Platz 7, 10587 Berlin, Germany        *
+%                                                                            *
+% Copyright (c) 2013      Institut fuer Nachrichtentechnik                   *
+%                         Universitaet Rostock                               *
+%                         Richard-Wagner-Strasse 31, 18119 Rostock           *
+%                                                                            *
+% This file is part of the Sound Field Synthesis-Toolbox (SFS).              *
+%                                                                            *
+% The SFS is free software:  you can redistribute it and/or modify it  under *
+% the terms of the  GNU  General  Public  License  as published by the  Free *
+% Software Foundation, either version 3 of the License,  or (at your option) *
+% any later version.                                                         *
+%                                                                            *
+% The SFS is distributed in the hope that it will be useful, but WITHOUT ANY *
+% WARRANTY;  without even the implied warranty of MERCHANTABILITY or FITNESS *
+% FOR A PARTICULAR PURPOSE.                                                  *
+% See the GNU General Public License for more details.                       *
+%                                                                            *
+% You should  have received a copy  of the GNU General Public License  along *
+% with this program.  If not, see <http://www.gnu.org/licenses/>.            *
+%                                                                            *
+% The SFS is a toolbox for Matlab/Octave to  simulate and  investigate sound *
+% field  synthesis  methods  like  wave  field  synthesis  or  higher  order *
+% ambisonics.                                                                *
+%                                                                            *
+% http://dev.qu.tu-berlin.de/projects/sfs-toolbox       sfstoolbox@gmail.com *
+%*****************************************************************************
 
 
 %% ===== Checking of input  parameters ==================================

e797b68e28ce4f842539055ed1dfb5878cbc5253
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2014-10-20 15:08:30 +0200

    Added new config settings to test_adaptivesecondarysources()

--- validation/test_adaptivesecondarysources.m
+++ validation/test_adaptivesecondarysources.m
@@ -2,11 +2,6 @@
 close all;
 clear variables;
 
-startup;
-
-% SFS Toolbox
-addpath('~/projects/sfstoolbox'); SFS_start;
-
 %% Parameters
 conf = SFS_config_example;
 conf.plot.useplot = false;
@@ -17,14 +12,13 @@ conf.plot.realloudspeakers = false;
 conf.usetapwin = true;
 
 % config for virtual array
-conf.localsfs.size = 9;
-conf.localsfs.center = [0, 0, 0];
-conf.localsfs.geometry = 'linear';
-
+conf.localsfs.vss.size = 9;
+conf.localsfs.vss.center = [0, 0, 0];
+conf.localsfs.vss.geometry = 'linear';
 conf.localsfs.vss.number = 20;
 conf.localsfs.vss.sampling = 'log';
-conf.localsfs.vss.method = 'wfs';
-conf.localsfs.vss.type = 'ls';
+conf.localsfs.method = 'wfs';
+% TODO: what does this?
 conf.localsfs.vss.ignoress = true;
 
 % config for real array
@@ -34,13 +28,13 @@ conf.secondary_sources.number = 56;
 conf.secondary_sources.size = 6;
 conf.secondary_sources.center = [0, 2, 0];
 conf.driving_functions = 'default';
-conf.xref = conf.localsfs.center;
+conf.xref = conf.localsfs.vss.center;
 
-X = conf.localsfs.center;
+X = conf.localsfs.vss.center;
 xs = [0, -1.0, 0];  % propagation direction of plane wave
 src = 'pw'; 
-xrange = [-conf.localsfs.size/2, conf.localsfs.size/2];
-yrange = [-conf.localsfs.size/2, conf.localsfs.size/2];
+xrange = [-conf.localsfs.vss.size/2, conf.localsfs.vss.size/2];
+yrange = [-conf.localsfs.vss.size/2, conf.localsfs.vss.size/2];
 zrange = 0;
 f = 1000;
 

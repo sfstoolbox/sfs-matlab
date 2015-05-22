eac52a65b99b4eb5e0e85a06f3adf2e2f3cf8030
Fiete Winter <fiete.winter@uni-rostock.de>
2015-05-22 12:49:57 +0200

    some renaming and moving of files

--- SFS_scattering/sphbasis_mono_grid.m
+++ SFS_scattering/sphbasis_mono_grid.m
@@ -1,28 +1,40 @@
-function [Jsph, H2sph, Ysph, x, y, z] = sphbasis_mono_XYZgrid(X,Y,Z,f,xq,conf)
-%Evaluate  basis functions for given grid in cartesian coordinates
+function [jn, h2n, Ynm, x, y, z] = sphbasis_mono_grid(X,Y,Z,Nse,f,xq,conf)
+%Evaluate spherical basis functions for given grid in cartesian coordinates
 %
 %
-%   Usage: [Jsph, H2sph, Ysph, x, y, z] = sphbasis_mono_XYZgrid(X,Y,Z,f,xq,conf)
+%   Usage: [jn, h2n, Ynm, x, y, z] = sphbasis_mono_grid(X,Y,Z,f,xq,conf)
 %
 %   Input parameters:
-%       X           - x-axis / m; single value or [xmin,xmax]
-%       Y           - y-axis / m; single value or [ymin,ymax]
-%       Z           - z-axis / m; single value or [zmin,zmax]
+%       X           - x-axis / m; single value or [xmin,xmax] or nD-array
+%       Y           - y-axis / m; single value or [ymin,ymax] or nD-array
+%       Z           - z-axis / m; single value or [zmin,zmax] or nD-array
+%       Nse         - maximum order of spherical basis functions
 %       f           - frequency in Hz
 %       xq          - optional center of coordinate system
 %       conf        - optional configuration struct (see SFS_config)
 %
 %   Output parameters:
-%       Jsph        - cell array of spherical bessel functions
-%       H2sph       - cell array of spherical hankel functions of 2nd kind
-%       Ysph        - cell array of spherical harmonics
+%       jn          - cell array of spherical bessel functions
+%       h2n         - cell array of spherical hankel functions of 2nd kind
+%       Ynm         - cell array of spherical harmonics
 %       x           - corresponding x axis / m
 %       y           - corresponding y axis / m
 %       z           - corresponding z axis / m
 %
-%   SPHBASIS_MONO_XYZGRID(X,Y,Z,f,xq,conf) computes spherical basis functions 
+%   SPHBASIS_MONO_GRID(X,Y,Z,f,xq,conf) computes spherical basis functions
 %   for given grid in cartesian coordinates. This is a wrapper function for
 %   sphbasis_mono.
+%   For the input of X,Y,Z (DIM as a wildcard) :
+%     * if DIM is given as single value, the respective dimension is
+%     squeezed, so that dimensionality of the simulated sound field P is
+%     decreased by one.
+%     * if DIM is given as [dimmin, dimmax], a linear grid for the
+%     respective dimension with a resolution defined in conf.resolution is
+%     established
+%     * if DIM is given as n-dimensional array, the other dimensions have
+%     to be given as n-dimensional arrays of the same size or as a single value.
+%     Each triple of X,Y,Z is interpreted as an evaluation point in an
+%     customized grid. For this option, plotting and normalisation is disabled.
 %
 %   see also: sphbasis_mono
 
@@ -59,25 +71,51 @@ function [Jsph, H2sph, Ysph, x, y, z] = sphbasis_mono_XYZgrid(X,Y,Z,f,xq,conf)
 %*****************************************************************************
 
 %% ===== Checking of input  parameters ==================================
-nargmin = 4;
-nargmax = 6;
+nargmin = 5;
+nargmax = 7;
 narginchk(nargmin,nargmax);
-isargvector(X,Y,Z);
-isargpositivescalar(f);
+
+isargnumeric(X,Y,Z);
+% unique index encoding which dimension is an nd-array
+customGrid = (numel(X) > 2) + 2*(numel(Y) > 2) + 4*(numel(Z) > 2);
+switch customGrid
+  case 1
+    isargscalar(Y,Z);
+  case 2
+    isargscalar(X,Z);
+  case 3
+    isargequalsize(X,Y); isargscalar(Z);
+  case 4
+    isargscalar(X,Y);
+  case 5
+    isargequalsize(X,Z); isargscalar(Y);
+  case 6
+    isargequalsize(Y,Z); isargscalar(X);
+  case 7
+    isargequalsize(X,Y,Z);
+  otherwise
+    isargvector(X,Y,Z);
+end
+isargpositivescalar(f,Nse);
 if nargin<nargmax
-    conf = SFS_config;
+  conf = SFS_config;
 else
-    isargstruct(conf);
+  isargstruct(conf);
 end
 if nargin == nargmin
   xq = [0, 0, 0];
-end  
+end
 isargposition(xq);
 
 %% ===== Computation ====================================================
-
-% Create a x-y-grid
-[xx,yy,zz,x,y,z] = xyz_grid(X,Y,Z,conf);
+if customGrid
+  % just copy everything
+  xx = X; yy = Y; zz = Z;
+  x = X;   y = Y;  z = Z;
+else
+  % Create a x-y-grid
+  [xx,yy,zz,x,y,z] = xyz_grid(X,Y,Z,conf);
+end
 
 k = 2*pi*f/conf.c;  % wavenumber
 
@@ -87,14 +125,10 @@ yy = yy-xq(2);
 zz = zz-xq(3);
 
 % coordinate transformation
-r = vector_norm(cat(3,xx,yy,zz),3);
+r = sqrt(xx.^2 + yy.^2 + zz.^2);
 phi = atan2(yy,xx);
 theta = asin(zz./r);
 
-[Jsph, H2sph, Ysph] = sphbasis_mono(r, theta, phi, k, conf);
+[jn, h2n, Ynm] = sphbasis_mono(r, theta, phi, Nse, k, conf);
 
 end
-
-
-
-

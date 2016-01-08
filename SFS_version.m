f63de28f9ce45f9d2a25455e53bf8a57b7640294
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-01-08 10:34:11 +0100

    Update NEWS and set version to 2.1.0

--- SFS_version.m
+++ SFS_version.m
@@ -43,4 +43,4 @@ function versionnumber = SFS_version()
 
 %% ===== Configuration ===================================================
 % Version number
-versionnumber = '2.0.0';
+versionnumber = '2.1.0';

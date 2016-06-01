29b73b716d1f5899a4875c3197b4e438a4ddfa4c
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-06-01 17:57:53 +0200

    DOC: Set master document to index.txt in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -55,7 +55,7 @@ source_suffix = '.txt'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'contents'
+master_doc = 'index'
 
 # General information about the project. (substitutions)
 project = 'SFS Toolbox - Documentation'

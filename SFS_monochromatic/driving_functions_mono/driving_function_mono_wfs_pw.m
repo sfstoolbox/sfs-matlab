d73411fa7fbf90e8b6bba4dd61a0da4caa46eae6
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2016-06-10 10:54:52 +0200

    Update WFS pw mono

--- SFS_monochromatic/driving_functions_mono/driving_function_mono_wfs_pw.m
+++ SFS_monochromatic/driving_functions_mono/driving_function_mono_wfs_pw.m
@@ -89,14 +89,10 @@ if strcmp('2D',dimension) || strcmp('3D',dimension)
         % D(x0,w) =  2 --- nk nx0  e^(-i w/c nk x0)
         %               c
         %
-        % see Wierstorf et al. (2015), eq.(#D:wfs:pw)
+        % See http://sfstoolbox.org/#equation-D.wfs.pw
         %
-        D = -2*1i*omega/c .* vector_product(nk,nx0,2) .*  ...
-            exp(-1i*omega/c.*vector_product(nk,x0,2));
-        %
-    elseif strcmp('delft1988',driving_functions)
-        % --- Delft 1988 -------------------------------------------------
-        to_be_implemented;
+        D = 2.*1i.*omega./c .* vector_product(nk,nx0,2) ...
+            .* exp(-1i.*omega./c.*vector_product(nk,x0,2));
         %
     else
         error(['%s: %s, this type of driving function is not implemented ', ...
@@ -106,70 +102,58 @@ if strcmp('2D',dimension) || strcmp('3D',dimension)
 
 elseif strcmp('2.5D',dimension)
 
-  % === 2.5-Dimensional ================================================
-  
-  % Reference point
-  xref = repmat(xref,[size(x0,1) 1]);
-  switch driving_functions
+    % === 2.5-Dimensional ================================================
+
+    % Reference point
+    xref = repmat(xref,[size(x0,1) 1]);
+    switch driving_functions
     case {'default', 'reference_point'}
-      % Driving function with only one stationary phase approximation, i.e.
-      % reference to one point in field
-      %        ______________
-      % g0 = \| 2pi |xref-x0|
-      %
-      g0 = sqrt( 2*pi*vector_norm(xref-x0,2) );
-      %
-      % D_2.5D using a plane wave as source model
-      %                             ___
-      %                            |i w|
-      % D_2.5D(x0,w) = g0 nk nx0 _ |---  e^(-i w/c nk x0)
-      %                           \| c
-      %
-      D = 2*g0 .* vector_product(nk,nx0,2) .* sqrt(1i*omega/c) .* ...
-        exp(-1i*omega/c.*vector_product(nk,x0,2));
+        % Driving function with only one stationary phase approximation, i.e.
+        % reference to one point in field
+        %        ______________
+        % g0 = \| 2pi |xref-x0|
+        %
+        g0 = sqrt( 2*pi*vector_norm(xref-x0,2) );
+        %
+        % D_2.5D using a plane wave as source model
+        %                             ___
+        %                            |i w|
+        % D_2.5D(x0,w) = g0 nk nx0 _ |---  e^(-i w/c nk x0)
+        %                           \| c
+        %
+        % See http://sfstoolbox.org/#equation-D.wfs.pw.2.5D
+        %
+        D = 2.*g0 .* vector_product(nk,nx0,2) .* sqrt(1i.*omega./c) ...
+            .* exp(-1i.*omega./c.*vector_product(nk,x0,2));
+        %
     case {'reference_line'}
-      % Driving function with two stationary phase approximations,
-      % reference to a line parallel to a LINEAR secondary source distribution
-      %
-      % distance ref-line to linear ssd
-      dref = vector_product(xref-x0,nx0,2);
-      % scalar product of plane wave and ssd normal ( npw * nx0 )
-      cospw = vector_product(nk,nx0,2);
-      %
-      % 2.5D correction factor
-      %        _____________________________
-      % g0 = \| 2pi * d_ref * ( npw * nx0 )
-      g0 = sqrt( 2*pi*dref.*cospw );
-      %
-      % D_2.5D using a plane wave as source model
-      %                        ___
-      %                       |i w|
-      % D_2.5D(x0,w) = 2* g0 _|---  e^(-i w/c nk x0)
-      %                      \| c
-      %
-      D = 2*g0 .* sqrt(1i*omega/c) .* exp( -1i*omega/c.*cospw );
-    case {'legacy'}
-      % --- SFS Toolbox ------------------------------------------------
-      % 2.5D correction factor
-      %        ______________
-      % g0 = \| 2pi |xref-x0|
-      %
-      g0 = sqrt(2*pi*vector_norm(xref-x0,2));
-      %
-      % D_2.5D using a plane wave as source model
-      %                               ___
-      %                              | w |
-      % D_2.5D(x0,w) = 2 g0 nk nx0 _ |---  e^(-i w/c nk x0)
-      %                             \|i c
-      %
-      % see Wierstorf et al. (2015), eq.(#D:wfs:pw:2.5D)
-      %
-      D = -2*g0 .* vector_product(nk,nx0,2) .* sqrt(omega/(1i*c)) .* ...
-        exp(-1i*omega/c.*vector_product(nk,x0,2));
+        % Driving function with two stationary phase approximations,
+        % reference to a line parallel to a LINEAR secondary source distribution
+        %
+        % Distance ref-line to linear ssd
+        dref = vector_product(xref-x0,nx0,2);
+        %
+        % 2.5D correction factor
+        %        ____________
+        % g0 = \| 2pi * d_ref
+        %
+        g0 = sqrt(2.*pi.*dref);
+        %
+        % D_2.5D using a plane wave as source model
+        %                       ___
+        %                      |i w|
+        % D_2.5D(x0,w) = 2g0 _ |---  e^(-i w/c nk x0)
+        %                     \| c
+        %
+        % See Schultz (2016), eq. (2.170)
+        %
+        D = 2.*g0 .* sqrt(1i*omega/c) .* sqrt(vector_product(xref-x0,nx0,2)) ...
+            .* exp(-1i.*omega./c.*vector_product(nk,x0,2));
+        %
     otherwise
-      error(['%s: %s, this type of driving function is not implemented ', ...
-        'for a 2.5D plane wave.'],upper(mfilename),driving_functions);
-  end
+        error(['%s: %s, this type of driving function is not implemented ', ...
+          'for a 2.5D plane wave.'],upper(mfilename),driving_functions);
+    end
 else
     error('%s: the dimension %s is unknown.',upper(mfilename),dimension);
 end

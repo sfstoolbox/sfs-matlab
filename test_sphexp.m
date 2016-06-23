a56e6d25654aae151821ed31f96bedd1a18e60c5
Fiete Winter <fiete.winter@uni-rostock.de>
2016-06-23 19:35:23 +0200

    rename some variables in test script for spherical expansions

--- test_sphexp.m
+++ test_sphexp.m
@@ -21,26 +21,28 @@ X = [-2 2];
 Y = [-2 2];
 Z = 0;
 
-f = 500;
-Nse = 10;
+f = 1000;
+Nse = 20;
 
 % scatterer
 sigma = inf;  % admittance of scatterer (inf to soft scatterer)
 R = 0.3;
-xq = [ 0, -1, 0];
-xt = [ 0.5, 0.5, 0];
+xq = [ 0, 0, 0];
+xt = [ 0.5, 0, 0];
 conf.xref = xq;
 
+
 %% Spherical Expansion
-% spherical expansion
-A1sph = sphexp_mono_pw(ns,Nse,f,xq,conf);
-A1sph_shift = sphexp_mono_pw(ns,Nse,f,xq+xt,conf);
-A2sph = sphexp_mono_ps(xs,'R',Nse, f,xq,conf);
-A2sph_shift = sphexp_mono_ps(xs,'R', Nse, f,xq+xt,conf);
-% regular-to-regular spherical reexpansion (translatory shift)
-[RRsph, RRsphm] = sphexp_mono_translation(xt, 'RR', Nse, f, conf);
-A1spht = RRsph*A1sph;
-A2spht = RRsph*A2sph;
+% regular spherical expansion at xq
+Apwsph = sphexp_mono_pw(ns,Nse,f,xq,conf);
+Apssph = sphexp_mono_ps(xs,'R',Nse, f,xq,conf);
+% regular spherical expansion at xq+xt (x' = x - xt)
+Apwsph_t = sphexp_mono_pw(ns,Nse,f,xq+xt,conf);
+Apssph_t = sphexp_mono_ps(xs,'R', Nse, f,xq+xt,conf);
+% regular-to-regular spherical reexpansion (translatory shift of -xt)
+[RRsph, RRsphm] = sphexp_mono_translation(-xt, 'RR', Nse, f, conf);
+A1sph_re = RRsph*Apwsph_t;
+A2sph_re = RRsph*Apssph_t;
 
 % Evaluate spherical basis functions 
 [jn, h2n, Ynm] = ...
@@ -50,37 +52,37 @@ A2spht = RRsph*A2sph;
   sphbasis_mono_grid(X, Y, Z,Nse,f,xq+xt,conf);
 
 % compute fields
-P1sph = sound_field_mono_sphbasis(A1sph, jn, Ynm);
-P1sph_shift = sound_field_mono_sphbasis(A1sph_shift, jnt, Ynmt);
-P1spht = sound_field_mono_sphbasis(A1spht, jnt, Ynmt);
-P2sph = sound_field_mono_sphbasis(A2sph, jn, Ynm);
-P2sph_shift = sound_field_mono_sphbasis(A2sph_shift, jnt, Ynmt);
-P2spht = sound_field_mono_sphbasis(A2spht, jnt, Ynmt);
+Ppwsph = sound_field_mono_sphbasis(Apwsph, jn, Ynm);
+Ppwsph_t = sound_field_mono_sphbasis(Apwsph_t, jnt, Ynmt);
+Ppwsph_re = sound_field_mono_sphbasis(A1sph_re, jn, Ynm);
+Ppssph = sound_field_mono_sphbasis(Apssph, jn, Ynm);
+Ppssph_t = sound_field_mono_sphbasis(Apssph_t, jnt, Ynmt);
+Ppssph_re = sound_field_mono_sphbasis(A2sph_re, jn, Ynm);
 
-plot_sound_field(P1sph ,X, Y, Z, [], conf);
+plot_sound_field(Ppwsph ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('plane wave');
-plot_sound_field(P1sph_shift ,X, Y, Z, [], conf);
+plot_sound_field(Ppwsph_t ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('plane wave (shifted expansion)');
-plot_sound_field(P1spht ,X, Y, Z, [], conf);
+plot_sound_field(Ppwsph_re ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('plane wave (shifted reexpansion)');
-plot_sound_field(P2sph ,X, Y, Z, [], conf);
+plot_sound_field(Ppssph ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('point source');
-plot_sound_field(P2sph_shift ,X, Y, Z, [], conf);
+plot_sound_field(Ppssph_t ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('point source (shifted expansion)');
-plot_sound_field(P2spht ,X, Y, Z, [], conf);
+plot_sound_field(Ppssph_re ,X, Y, Z, [], conf);
 plot_scatterer(xq,R);
 title('point source (shifted reexpansion)');
 
 %% Scattering with Sphere
 % scatterer is assumed to be located at coordinates origin
-B1sph = sphexp_mono_scatter(A1sph, R, sigma, f, conf); 
-B2sph = sphexp_mono_scatter(A2sph, R, sigma, f, conf);
+B1sph = sphexp_mono_scatter(Apwsph, R, sigma, f, conf); 
+B2sph = sphexp_mono_scatter(Apssph, R, sigma, f, conf);
 
 % compute fields
-P1sph_scatter = sound_field_mono_sphbasis(B1sph, h2n, Ynm);
-P2sph_scatter = sound_field_mono_sphbasis(B2sph, h2n, Ynm);
+Ppwsph_scatter = sound_field_mono_sphbasis(B1sph, h2n, Ynm);
+Ppssph_scatter = sound_field_mono_sphbasis(B2sph, h2n, Ynm);

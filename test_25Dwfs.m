eb4e0a46b2aa5acdbd3f08bcd4e6c22d1950f255
Fiete Winter <fiete.winter@uni-rostock.de>
2016-06-29 14:22:46 +0200

    improve test script

--- test_25Dwfs.m
+++ test_25Dwfs.m
@@ -7,17 +7,17 @@ conf.plot.useplot = false;
 conf.usenormalisation = false;
 
 conf.secondary_sources.geometry = 'linear';
-conf.secondary_sources.size = 100;
-conf.secondary_sources.number = 1000;
-conf.secondary_sources.center = [0,5,0];
+conf.secondary_sources.size = 20;
+conf.secondary_sources.number = 512;
+conf.secondary_sources.center = [0,6,0];
 conf.xref = [0,0,0];
 
 conf.usetapwin = true;
 conf.tapwinlen = 0.2;
 
 %%
-f = 1000;  % temporal frequency
-positions = { [0,7,0], [-sqrt(0.5), -sqrt(0.5), 0], [0,3,0,0,-1,0] };  % source positions
+f = 2000;  % temporal frequency
+positions = { [0,9,0], [0,-1,0], [0,3,0,0,-1,0] };  % source positions
 sources = {'ps', 'pw', 'fs'};
 gtsources = {'ps', 'pw', 'ps'};
 
@@ -30,17 +30,27 @@ for idx=1:length(positions)
   src = sources{idx};
   gt = gtsources{idx};
   
+  figure;
+  ddx = 1;
   for driving_functions = {'reference_point', 'reference_line'}
     conf.driving_functions = driving_functions{:};
 
     [Pgt,x,y] = sound_field_mono(X,Y,Z,[xs(1:3),0,-1,0,1], gt, 1, f, conf);
     Pwfs = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
     
-    figure;
+    subplot(1,2,ddx);
     imagesc(Y,X,db(1 - Pwfs./Pgt));    
     title(sprintf('%s %s', src, driving_functions{:}), 'Interpreter', 'none');
     set(gca, 'YDir', 'normal');
+    colorbar;
+    hold on;
+    if strcmp( conf.driving_functions, 'reference_point' )
+       plot(conf.xref(1),conf.xref(2),'gx');
+    else
+       plot(conf.xref(1)+X,conf.xref([2,2]),'g--');
+    end    
+    hold off;
     
-    % plot_sound_field(Pwfs./Pgt, X, Y, Z, [], conf);   
-  end 
+    ddx= ddx+1;
+  end     
 end

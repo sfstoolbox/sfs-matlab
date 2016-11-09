fc7047998122c6ea56d32d2ceacab32557c274df
VeraE <vera.erbes@uni-rostock.de>
2016-11-09 18:14:00 +0100

    Add amplitude terms to FIR prefilter

--- SFS_time_domain/wfs_fir_prefilter.m
+++ SFS_time_domain/wfs_fir_prefilter.m
@@ -55,7 +55,8 @@ isargstruct(conf);
 
 %% ===== Configuration ==================================================
 fs = conf.fs;                  % Sampling rate
-dimension = conf.dimension;    % dimensionality
+c = conf.c;                    % Speed of sound
+dimension = conf.dimension;    % Dimensionality
 flow = conf.wfs.hpreflow;      % Lower frequency limit of preequalization
                                % filter (= frequency when subwoofer is active)
 fhigh = conf.wfs.hprefhigh;    % Upper frequency limit of preequalization
@@ -92,19 +93,21 @@ if strcmp('2.5D',dimension)
     %
     %  See http://sfstoolbox.org/#equation-h.wfs.2.5D
     %
-    H(idxflow:idxfhigh) = sqrt(f(idxflow:idxfhigh)./fhigh);
+    H(idxflow:idxfhigh) = sqrt(2*pi*f(idxflow:idxfhigh)/c);
+    H(idxfhigh:end) = H(idxfhigh);
 elseif strcmp('3D',dimension) || strcmp('2D',dimension)
     %
     %  H(f) = f/fhigh, for flow<=f<=fhigh
     %
     %  See http://sfstoolbox.org/#equation-h.wfs
     %
-    H(idxflow:idxfhigh) = f(idxflow:idxfhigh)./fhigh;
+    H(idxflow:idxfhigh) = 2*pi*f(idxflow:idxfhigh)/c;
+    H(idxfhigh:end) = H(idxfhigh);
 else
     error('%s: %s is not a valid conf.dimension entry',upper(mfilename),dimension);
 end
 % Set the response for idxf < idxflow to the value at idxflow
-H(1:idxflow) = H(idxflow)*ones(1,idxflow);
+H(1:idxflow) = H(idxflow);
 
 % Compute filter
 hpre = firls(Nfilt,2*f/fs,H);

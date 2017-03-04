882066f0ce27837828c39d38dbe244e98df3452e
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-03-04 11:21:54 +0100

    Bump version number

--- SFS_version.m
+++ SFS_version.m
@@ -40,4 +40,4 @@ function versionnumber = SFS_version()
 
 %% ===== Configuration ===================================================
 % Version number
-versionnumber = '2.3.0';
+versionnumber = '2.4.0';

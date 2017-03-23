6bc090809cabb61ff0cfa0f888907f9ec87758eb
Fiete Winter <fiete.winter@uni-rostock.de>
2017-03-23 18:04:55 +0100

    fix license text

--- SFS_time_domain/driving_functions_imp/driving_function_imp_localwfs_sbl_pw.m
+++ SFS_time_domain/driving_functions_imp/driving_function_imp_localwfs_sbl_pw.m
@@ -1,27 +1,32 @@
 function [d, delay_offset] = driving_function_imp_localwfs_sbl_pw(x0, nk, conf)
 
 %*****************************************************************************
-% Copyright (c) 2016      Fiete Winter                                       *
-%                         Institut fuer Nachrichtentechnik                   *
-%                         Universitaet Rostock                               *
-%                         Richard-Wagner-Strasse 31, 18119 Rostock, Germany  *
+% The MIT License (MIT)                                                      *
 %                                                                            *
-% This file is part of the supplementary material for Fiete Winter's         *
-% scientific work and publications                                           *
+% Copyright (c) 2010-2017 SFS Toolbox Developers                             *
 %                                                                            *
-% You can redistribute the material and/or modify it  under the terms of the *
-% GNU  General  Public  License as published by the Free Software Foundation *
-% , either version 3 of the License,  or (at your option) any later version. *
+% Permission is hereby granted,  free of charge,  to any person  obtaining a *
+% copy of this software and associated documentation files (the "Software"), *
+% to deal in the Software without  restriction, including without limitation *
+% the rights  to use, copy, modify, merge,  publish, distribute, sublicense, *
+% and/or  sell copies of  the Software,  and to permit  persons to whom  the *
+% Software is furnished to do so, subject to the following conditions:       *
 %                                                                            *
-% This Material is distributed in the hope that it will be useful, but       *
-% WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY *
-% or FITNESS FOR A PARTICULAR PURPOSE.                                       *
-% See the GNU General Public License for more details.                       *
+% The above copyright notice and this permission notice shall be included in *
+% all copies or substantial portions of the Software.                        *
 %                                                                            *
-% You should  have received a copy of the GNU General Public License along   *
-% with this program. If not, see <http://www.gnu.org/licenses/>.             *
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+% IMPLIED, INCLUDING BUT  NOT LIMITED TO THE  WARRANTIES OF MERCHANTABILITY, *
+% FITNESS  FOR A PARTICULAR  PURPOSE AND  NONINFRINGEMENT. IN NO EVENT SHALL *
+% THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+% LIABILITY, WHETHER  IN AN  ACTION OF CONTRACT, TORT  OR OTHERWISE, ARISING *
+% FROM,  OUT OF  OR IN  CONNECTION  WITH THE  SOFTWARE OR  THE USE  OR OTHER *
+% DEALINGS IN THE SOFTWARE.                                                  *
 %                                                                            *
-% http://github.com/fietew/publications           fiete.winter@uni-rostock.de*
+% The SFS Toolbox  allows to simulate and  investigate sound field synthesis *
+% methods like wave field synthesis or higher order ambisonics.              *
+%                                                                            *
+% http://sfstoolbox.org                                 sfstoolbox@gmail.com *
 %*****************************************************************************
 
 %% ===== Checking of input  parameters ==================================

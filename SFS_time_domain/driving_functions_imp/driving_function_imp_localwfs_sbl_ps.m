7b955e39ee3c2324cbe9d346f3172bea13f683cd
Fiete Winter <fiete.winter@uni-rostock.de>
2017-03-24 18:18:31 +0100

    add allpass filtering to compensate phase distortion

--- SFS_time_domain/driving_functions_imp/driving_function_imp_localwfs_sbl_ps.m
+++ SFS_time_domain/driving_functions_imp/driving_function_imp_localwfs_sbl_ps.m
@@ -29,7 +29,7 @@ function [d,delay_offset] = driving_function_imp_localwfs_sbl_ps(x0,xs,conf)
 % http://sfstoolbox.org                                 sfstoolbox@gmail.com *
 %*****************************************************************************
 
-%% ===== Checking of input  parameters ==================================
+%% ===== Checking of input  parameters ========================================
 nargmin = 3;
 nargmax = 3;
 narginchk(nargmin,nargmax);
@@ -39,7 +39,6 @@ N0 = size(x0,1);
 Nfft = conf.N;
 xref = conf.xref;
 fs = conf.fs;
-c = conf.c;
 % Ambisonics order
 if isempty(conf.localsfs.sbl.order)
     Nce = nfchoa_order(N0,conf);
@@ -58,52 +57,36 @@ if isempty(conf.localsfs.sbl.Npw)
 else
   Npw = conf.localsfs.sbl.Npw;
 end
-driving_functions = conf.driving_functions;
 
 conf.t0 = 'source';  % needed to time-align lf and hf part of driving function
 wfsconf = conf;
 wfsconf.wfs = conf.localsfs.wfs;
 wfsconf.driving_functions = conf.localsfs.sbl.driving_functions;
 
+%% ===== Variables ============================================================
+Nlr = ceil(Nce/2)*2;  % order of Linkwitz-Riley Coefficients
+Wlr = fc/fs*2;  % normalised cut-off frequency of Linkwitz-Riley
+
 %% ===== Computation ==========================================================
 
 % === Local WFS for high frequencies ===
-switch driving_functions
-case {'default', 'hp-filtered'}
-  % regular circular expansion of point source (highpass implicitly)
-  [pm, delay_circexp] = circexp_imp_ps(xs, Nce, xref, fc, conf);
-  % coefficient for lowpass filtering of WFS driving function
-  [zlp, plp, klp] = linkwitz_riley(ceil(Nce/2)*2, fc/fs*2, 'low');  % lr-filter
-case 'hf-approximation'
-  % regular circular expansion of point source (high-frequency approximation)
-  rs = norm(xs - xref);
-  ns = -(xs-xref)./rs;
-  %
-  pm = circexp_imp_pw(ns, Nce, xref, conf);
-  pm = pm/(4*pi*rs);
-  %
-  delay_circexp = -rs/c;
-  % highpass filtering
-  [zhp, php, khp] = butter(1, fc/fs*2, 'high');
-  [sos, g] = zp2sos(zhp, php, khp, 'down', 'none');  % generate sos
-  pm = sosfilt(sos, pm, 2)*g;
-  % coefficient for lowpass filtering of WFS driving function
-  [zlp, plp, klp] = butter(1, fc/fs*2, 'low');
-end
+% regular circular expansion of point source (highpass implicitly)
+[pm, delay_circexp] = circexp_imp_ps(xs, Nce, xref, fc, conf);
 % plane wave decomposition
 ppwd = pwd_imp_circexp(pm, Npw);
 % driving signal
 [d_lwfs, delay_lwfs] = driving_function_imp_wfs_pwd(x0, ppwd, xref, wfsconf);
 
 % === WFS for low frequencies ===
-% 
-conf.driving_functions = 'reference_point';
 % secondary source selection
 [~, xdx] = secondary_source_selection(x0, xs, 'ps');
 x0(xdx,:) = secondary_source_tapering(x0(xdx,:) , conf);
 % driving function
 d_lp = zeros(Nfft,N0);
-[d_lp(:,xdx), ~, ~, delay_lp] = driving_function_imp_wfs(x0(xdx,:), xs, 'ps', conf);
+[d_lp(:,xdx), ~, ~, delay_lp] = ...
+  driving_function_imp_wfs(x0(xdx,:), xs, 'ps', conf);
+% coefficients for lowpass filtering of WFS driving function
+[zlp, plp, klp] = linkwitz_riley(Nlr, Wlr, 'low');  % lr-filter
 % lowpass filtering
 [sos, g] = zp2sos(zlp, plp, klp, 'down', 'none');  % generate sos
 d_lp = sosfilt(sos, d_lp, 1)*g;
@@ -113,7 +96,16 @@ d_lp = sosfilt(sos, d_lp, 1)*g;
 [~, delay_delayline] = delayline(1, 0, 0, conf);
 % delay to compensate between lf-part and hf-part
 delay_comp = (delay_lp-delay_lwfs-delay_circexp)*conf.fs - delay_delayline;
-% final driving signal
+% combined driving signal
 d = d_lp + delayline(d_lwfs, delay_comp, 1, conf);
+
+% === Compensate Phase-Distortions ===
+% coefficients for allpass filtering
+[zap, pap, kap] = linkwitz_riley(Nlr, Wlr, 'all');
+[sos, g] = zp2sos(zap, pap, kap, 'down', 'none');  % generate sos
+% (time-reversed) allpass filtering
+d = sosfilt(sos, d(end:-1:1,:), 1)*g;
+d = d(end:-1:1,:);
+
 % final delay
 delay_offset = delay_lp;

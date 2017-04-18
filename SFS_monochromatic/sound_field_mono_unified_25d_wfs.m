916fc76cb3d3d9f3afbeb64b63671b28bf71dbc6
Frank Schultz <scf175@googlemail.com>
2017-04-18 13:04:05 +0200

    added test script for unified 2.5D WFS, added sound_field_mono_unified_25d_wfs() to work with driving functions

---
+++ SFS_monochromatic/sound_field_mono_unified_25d_wfs.m
@@ -0,0 +1,105 @@
+function varargout = sound_field_mono_unified_25d_wfs(X,Y,Z,xs,src,f,conf,dx0)
+%SOUND_FIELD_MONO_WFS simulates a sound field for WFS
+%
+%   Usage: [P,x,y,z,x0] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf)
+%
+%   Input parameters:
+%       X           - x-axis / m; single value or [xmin,xmax] or nD-array
+%       Y           - y-axis / m; single value or [ymin,ymax] or nD-array
+%       Z           - z-axis / m; single value or [zmin,zmax] or nD-array
+%       xs          - position of virtual source / m
+%       src         - source type of the virtual source
+%                         'pw' - plane wave (xs is the direction of the
+%                                plane wave in this case)
+%                         'ps' - point source
+%                         'fs' - focused source
+%       f           - monochromatic frequency / Hz
+%       conf        - configuration struct (see SFS_config)
+%       dx0         - amplitude factor of unified 2.5D WFS framework
+%                     (i.e. the referencing function)
+%                     to obtain amplitude correct synthesis at desired
+%                     locations, i.e. along a definable reference curve,
+%                     this is primary source AND x0 dependent,
+%                     one dx0 per one x0
+%
+%   Output parameters:
+%       P           - simulated sound field
+%       x           - corresponding x values / m
+%       y           - corresponding y values / m
+%       z           - corresponding z values / m
+%       x0          - active secondary sources / m
+%       xPCS        - positions/locations of amplitude correct synthesis
+%                     i.e. the reference curve
+%   SOUND_FIELD_MONO_WFS(X,Y,Z,xs,src,f,conf) simulates a monochromatic sound
+%   field for the given source type (src) synthesized with wave field synthesis.
+%
+%   To plot the result use:
+%   plot_sound_field(P,X,Y,Z,x0,conf);
+%   or simple call the function without output argument:
+%   sound_field_mono_wfs(X,Y,Z,xs,src,f,conf)
+%
+%   See also: plot_sound_field, sound_field_imp_wfs, driving_function_mono_wfs
+
+%*****************************************************************************
+% The MIT License (MIT)                                                      *
+%                                                                            *
+% Copyright (c) 2010-2017 SFS Toolbox Developers                             *
+%                                                                            *
+% Permission is hereby granted,  free of charge,  to any person  obtaining a *
+% copy of this software and associated documentation files (the "Software"), *
+% to deal in the Software without  restriction, including without limitation *
+% the rights  to use, copy, modify, merge,  publish, distribute, sublicense, *
+% and/or  sell copies of  the Software,  and to permit  persons to whom  the *
+% Software is furnished to do so, subject to the following conditions:       *
+%                                                                            *
+% The above copyright notice and this permission notice shall be included in *
+% all copies or substantial portions of the Software.                        *
+%                                                                            *
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+% IMPLIED, INCLUDING BUT  NOT LIMITED TO THE  WARRANTIES OF MERCHANTABILITY, *
+% FITNESS  FOR A PARTICULAR  PURPOSE AND  NONINFRINGEMENT. IN NO EVENT SHALL *
+% THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+% LIABILITY, WHETHER  IN AN  ACTION OF CONTRACT, TORT  OR OTHERWISE, ARISING *
+% FROM,  OUT OF  OR IN  CONNECTION  WITH THE  SOFTWARE OR  THE USE  OR OTHER *
+% DEALINGS IN THE SOFTWARE.                                                  *
+%                                                                            *
+% The SFS Toolbox  allows to simulate and  investigate sound field synthesis *
+% methods like wave field synthesis or higher order ambisonics.              *
+%                                                                            *
+% http://sfstoolbox.org                                 sfstoolbox@gmail.com *
+%*****************************************************************************
+
+
+%% ===== Checking of input  parameters ==================================
+nargmin = 8;
+nargmax = 8;
+narginchk(nargmin,nargmax);
+isargxs(xs);
+isargpositivescalar(f);
+isargchar(src);
+isargstruct(conf);
+
+
+%% ===== Configuration ==================================================
+if strcmp('2D',conf.dimension)
+    greens_function = 'ls';
+else
+    greens_function = 'ps';
+end
+
+
+%% ===== Computation ====================================================
+% Get the position of the loudspeakers and its activity
+x0 = secondary_source_positions(conf);
+x0 = secondary_source_selection(x0,xs,src);
+x0 = secondary_source_tapering(x0,conf);
+
+%%
+% Driving function
+[D, xPCS] = driving_function_mono_unified_25d_wfs(x0,xs,dx0,src,f,conf);
+% Wave field
+[varargout{1:min(nargout,4)}] = ...
+    sound_field_mono(X,Y,Z,x0,greens_function,D,f,conf);
+% Return secondary sources if desired
+if nargout==5, varargout{5}=x0; end
+if nargout==6, varargout{5}=x0; varargout{6}=xPCS; end

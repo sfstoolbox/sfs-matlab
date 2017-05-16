77f3450b2b0e1f451d742c174037252eb7a38a2d
Fiete Winter <fiete.winter@uni-rostock.de>
2017-05-16 13:28:41 +0200

    add documentation about modal windows

--- doc/img/generate_plots.m
+++ doc/img/generate_plots.m
@@ -189,12 +189,6 @@ conf = SFS_config;
 conf.dimension = '2.5D';
 conf.plot.useplot = true;
 % sound_field_imp_nfchoa(X,Y,Z,xs,src,t,conf)
-conf.modal_window = 'max-rE';
-sound_field_imp_nfchoa([-2 2],[-2 2],0,[0 2 0],'ps',0.005,conf);
-axis([-2 2 -2 2]);
-set(gca, 'XTick', -2:1:2); set(gca, 'YTick', -2:1:2);
-print_png('sound_field_imp_nfchoa_25d_max-rE.png');
-conf.modal_window = 'rect';
 [p,x,y,z,x0] = sound_field_imp_nfchoa([-2 2],[-2 2],0,[0 2 0],'ps',0.005,conf);
 axis([-2 2 -2 2]);
 set(gca, 'XTick', -2:1:2); set(gca, 'YTick', -2:1:2);
@@ -244,6 +238,33 @@ sound_field_imp_nfchoa(X,Y,0,[0 2 0],'ps',0.005,conf);
 axis([-2 2 -2 2]);
 set(gca, 'XTick', -2:1:2); set(gca, 'YTick', -2:1:2);
 print_png('sound_field_imp_nfchoa_25d_dB_custom_grid.png');
+
+%% ===== modal windows ===================================================
+conf = SFS_config;
+conf.dimension = '2.5D';
+conf.secondary_sources.number = 16;
+conf.secondary_sources.geometry = 'circular';
+conf.secondary_sources.size = 3;
+conf.resolution = 300;
+conf.plot.usedb = true;
+conf.t0 = 'source';
+X = [-2,2];
+Y = [-2,2];
+Z = 0;
+conf.modal_window = 'rect';  % default
+sound_field_imp_nfchoa(X,Y,Z,[0 -1 0],'pw',0,conf);
+print_png('sound_field_imp_nfchoa_25d_dB_rect.png');
+conf.modal_window = 'max-rE';
+sound_field_imp_nfchoa(X,Y,Z,[0 -1 0],'pw',0,conf);
+print_png('sound_field_imp_nfchoa_25d_dB_max-rE.png');
+conf.modal_window = 'kaiser';
+conf.modal_window_parameter = 1.0;
+sound_field_imp_nfchoa(X,Y,Z,[0 -1 0],'pw',0,conf);
+print_png('sound_field_imp_nfchoa_25d_dB_kaiser.png');
+conf.modal_window = 'tukey';
+conf.modal_window_parameter = 0.5;
+sound_field_imp_nfchoa(X,Y,Z,[0 -1 0],'pw',0,conf);
+print_png('sound_field_imp_nfchoa_25d_dB_tukey.png');
 
 
 %% ===== impulse response of a spatial audio system ======================

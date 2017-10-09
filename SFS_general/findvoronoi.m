ab9eeed4fe4cf4f685398b7db72948ce2de6da18
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-10-09 18:04:31 +0100

    Small coding style updates

--- SFS_general/findvoronoi.m
+++ SFS_general/findvoronoi.m
@@ -1,7 +1,7 @@
 function [idx,weights] = findvoronoi(x0,xs)
 %FINDVORONOI finds the corresponding voronoi regions to the points x0
-% surrounding the desired point xs. The weights are derived from the voronoi
-% region surface area differences with and without xs.
+%surrounding the desired point xs. The weights are derived from the Voronoi
+%region surface area differences with and without xs.
 %
 %   Usage: [idx,weights] = findvoronoi(x0,xs)
 %
@@ -13,7 +13,6 @@ function [idx,weights] = findvoronoi(x0,xs)
 %       idx         - row indices of N points in x0 [Nx1]
 %       weights     - weights [Nx1]
 %
-%   
 %
 %   See also: findnearestneighbour, findconvexcone,
 %             test_interpolation_point_selection
@@ -53,6 +52,7 @@ nargmin = 2;
 nargmax = 2;
 narginchk(nargmin,nargmax);
 
+
 %% ===== Prepare Grid ====================================================
 % Normalise x0 and xs to lie on unit sphere
 xs = xs./norm(xs,2);
@@ -66,7 +66,7 @@ x0 = x0./repmat(radii,[1,size(x0,2)]);
 center = [0 0 0];
 
 % In case xs is colinear with or equals 1 point there is no interpolation needed
-eq_idx = findrows(x0, xs);
+eq_idx = findrows(x0,xs);
 if ~isempty(eq_idx) && size(eq_idx,1)<2
     weights = 1;
     idx = eq_idx;
@@ -76,12 +76,12 @@ elseif ~isempty(eq_idx) && ~size(eq_idx,1)<2
 end
 
 % Rotate to principal axes to enable 2D arrays
-[x0, xs, status_2d] = rotate_to_principal_axes(x0, xs);
+[x0,xs,is_2d] = rotate_to_principal_axes(x0,xs);
 
 % Calculate dummy points to enable "partial" arrays
 dummy_points = augment_bounding_box(x0);
-if status_2d % Add dummy points in 2D case
-    dummy_points = cat(1,dummy_points,[[0 0 1];[0 0 -1]]);
+if is_2d % Add dummy points in 2D case
+    dummy_points = cat(1,dummy_points,[[0 0 1]; [0 0 -1]]);
 end
 if ~isempty(dummy_points)
     dummy_indices = (1:size(dummy_points,1)) + size(x0,1);
@@ -98,23 +98,22 @@ xs_idx = size([x0;xs], 1);
 [row, ~] = find(simplices_new == xs_idx);
 xs_tri = simplices_new(row, :);
 idx = unique(xs_tri(xs_tri ~= xs_idx));
-% x0_s = x0(idx, :);
 
 % Extract all triangles from the simplices with at least one x0_s as a vertex
 [simplices_new_s, simplices_old_s] = deal([]);
 for n = 1:size(idx)
     [row, ~] = find(simplices_new == idx(n));
     [row_old, ~] = find(simplices_old == idx(n));
-    simplices_new_s = cat(1, simplices_new_s, simplices_new(row, :));
-    simplices_old_s = cat(1, simplices_old_s, simplices_old(row_old, :));
+    simplices_new_s = cat(1,simplices_new_s,simplices_new(row,:));
+    simplices_old_s = cat(1,simplices_old_s,simplices_old(row_old,:));
 end
-simplices_new_s = unique(simplices_new_s, 'rows');
-simplices_old_s = unique(simplices_old_s, 'rows');
+simplices_new_s = unique(simplices_new_s,'rows');
+simplices_old_s = unique(simplices_old_s,'rows');
 
 % Compute spherical voronoi_regions for each x0_s
-[regions_new_s, vertices_new_s] = calc_voronoi_regions([x0;xs], center, ...
+[regions_new_s, vertices_new_s] = calc_voronoi_regions([x0;xs],center, ...
     simplices_new_s);
-[regions_old_s, vertices_old_s] = calc_voronoi_regions([x0;xs], center, ...
+[regions_old_s, vertices_old_s] = calc_voronoi_regions([x0;xs],center, ...
     simplices_old_s);
 
 % Prepare regions for calculation of the surface area
@@ -128,9 +127,9 @@ simplices_old_s = unique(simplices_old_s, 'rows');
 [area_new, area_old] = deal(zeros(size(idx,1),1));
 for i=1:size(idx,1)
     area_new(i) = calc_surface_area( ...
-        vertices_new_s(regions_new_s{idx(i)},:), 1);
+        vertices_new_s(regions_new_s{idx(i)},:),1);
     area_old(i) = calc_surface_area( ...
-        vertices_old_s(regions_old_s{idx(i)},:), 1);    
+        vertices_old_s(regions_old_s{idx(i)},:),1);    
 end
 
 % Calculate weights
@@ -155,12 +154,13 @@ weights = weights./sum(weights);
 idx = idx(order);
 end
 
-% =========================================================================
 
-function [regions, vertices] = calc_voronoi_regions(x0, center, simplices)
-%CALC_VORONOI_REGIONS calculates the voronoi vertices and regions of the given
-% points x0. In case specific simplices are provided as input argument, only
-% those will be included in the calculation.
+%% ===== Functions =======================================================
+
+function [regions,vertices] = calc_voronoi_regions(x0,center,simplices)
+%CALC_VORONOI_REGIONS calculates the Voronoi vertices and regions of the given
+%points x0. In case specific simplices are provided as input argument, only
+%those will be included in the calculation.
 %
 %   Input parameters:
 %       x0             - point cloud on the surface of a unit sphere in R^3 [nx3]
@@ -171,30 +171,30 @@ function [regions, vertices] = calc_voronoi_regions(x0, center, simplices)
 %   Output parameters:
 %       regions        - cell array consisting of the indices of the vertices
 %                        to belong to the region of each point of x0 [nx3]
-%       vertices       - voronoi vertices in R^3 [Mx3]
+%       vertices       - Voronoi vertices in R^3 [Mx3]
 %
 %   Code based on scipy.spatial._spherical_voronoi.SphericalVoronoi
 
 if nargin < 3
-    simplices = convhulln(x0); % In case simplices are not provided as input arg
+    simplices = convhulln(x0); % in case simplices are not provided as input arg
 end
 
-% Tetrahedrons from Delaunay triangulation with shape [4x3x2n-4]
+% Tetrahedrons from Delaunay triangulation with shape [4x3x2n-4].
 % Add center of sphere to each of the simplices
 tri = x0(simplices.', 1:end);
-tetrahedrons=[];
+tetrahedrons = [];
 for n=1:3:size(tri,1);
-    tetrahedrons=cat(3,tetrahedrons,cat(1,tri(n:n+2,:),center));
+    tetrahedrons = cat(3,tetrahedrons,cat(1,tri(n:n+2,:),center));
 end
 
 % Calculate circumcenters of tetrahedrons
-circumcenters=calc_circumcenters(tetrahedrons);
+circumcenters = calc_circumcenters(tetrahedrons);
 
 % Project circumcenters of the tetrahedrons to the surface of the unit
 % sphere and thereby get the voronoi vertices with shape [2n-4x3]
-vertices = bsxfun(@rdivide, circumcenters, vector_norm(circumcenters,2));
+vertices = bsxfun(@rdivide,circumcenters,vector_norm(circumcenters,2));
 
-% Calculate regions from triangulation
+% Calculate regions from triangulation.
 % simplex_indices have shape [1x2n-4]
 simplex_indices = 1:1:size(simplices,1);
 % tri_indices have shape [1x6n-12]
@@ -215,7 +215,7 @@ end
 
 function [circumcenters] = calc_circumcenters(tetrahedrons)
 %CALC_CIRCUMCENTERS calculates the circumcenters of the circumspheres of 
-% tetrahedrons.
+%tetrahedrons.
 %
 %   Input parameters:
 %       tetrahedrons   - tetrahedrons defined by 4 points in R^3 [4x2xN]
@@ -254,10 +254,10 @@ end
 
 % =========================================================================
 
-function [ regions_sorted ] = sort_voronoi_vertices_of_regions( simplices, regions )
-%SORT_VORONOI_VERTICES_OF_REGIONS sorts the indices of the voronoi vertices for
-% each region such that the resulting points are in a clockwise or
-% counterclockwise order around the generator point.
+function [regions_sorted] = sort_voronoi_vertices_of_regions(simplices,regions)
+%SORT_VORONOI_VERTICES_OF_REGIONS sorts the indices of the Voronoi vertices for
+%each region such that the resulting points are in a clockwise or
+%counterclockwise order around the generator point.
 %
 %   Input parameters:
 %       simplices      - array of shape (Nx3) consisting of N points on 
@@ -344,9 +344,9 @@ end
 
 % =========================================================================
 
-function [ surface_area ] = calc_surface_area( vertices, radius )
+function [surface_area] = calc_surface_area(vertices,radius)
 %CALC_SURFACE_AREA calculates the surface area of a polygon on the surface of a
-% sphere. The input vertices need to be sorted around their generator point.
+%sphere. The input vertices need to be sorted around their generator point.
 %
 %   Input parameters:
 %       vertices       - sorted vertices [Nx3]
@@ -383,9 +383,9 @@ end
 
 % =========================================================================
 
-function [ sph_dist ] = calc_haversine_dist( x1, x2 )
+function [sph_dist] = calc_haversine_dist(x1,x2)
 %CALC_HAVERSINE_DISTANCE calculates the distance between two points on the 
-% surface of a unit sphere based on the haversine formula provided here: 
+% surface of a unit sphere based on the Haversine formula provided here: 
 % https://en.wikipedia.org/wiki/Haversine_formula
 %
 %   Code based on: voronoi_utility.py from https://doi.org/10.5281/zenodo.13688
@@ -403,10 +403,10 @@ end
 
 % =========================================================================
 
-function [x0, xs, status_2d] = rotate_to_principal_axes(x0, xs, gamma)
+function [x0,xs,is_2d] = rotate_to_principal_axes(x0,xs,gamma)
 %ROTATE_TO_PRINCIPAL_AXES rotates x0 and xs to x0's principal axes.
-% If the ratio of second-to-smallest singular values is < gamma,
-% the third dimension of x0 is set to zero.
+%If the ratio of second-to-smallest singular values is < gamma,
+%the third dimension of x0 is set to zero.
 %
 %   Input parameters:
 %       x0          - point cloud in R^3 [nx3]
@@ -416,11 +416,11 @@ function [x0, xs, status_2d] = rotate_to_principal_axes(x0, xs, gamma)
 %   Output parameters:
 %       x0          - point cloud in R^3
 %       xs          - point in R^3
-%       status_2d   - true or false
+%       is_2d       - true or false
 if nargin < 3
     gamma = 0.1; % inverse of aspect ratio of principal axes
 end
-status_2d = 0;
+is_2d = false;
 
 [~,S,V] = svd(x0);
 x0 = x0*V;
@@ -428,8 +428,7 @@ xs = xs*V;
 S = diag(S);
 if S(end)/S(end-1) < gamma
      x0(:,3) = 0;
-     %xs(3) = 0;
-    status_2d = 1;
+    is_2d = true;
     warning('SFS:findconvexcone','%s: Grid is apparently two-dimensional. ', ...
         upper(mfilename));
 end
